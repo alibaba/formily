1e5325f424a4821ba471aaabcb8d8860d494db82
janryWang <wangzhili56@126.com>
2019-02-22 21:39:30 +0800

    refactor(project): add test watch script

--- scripts/build.js
+++ scripts/build.js
@@ -5,6 +5,7 @@ const chalk = require('chalk')
 const { transformFileAsync } = require('@babel/core')
 const json5 = require('json5')
 const execa = require('execa')
+const chokidar = require('chokidar')
 const cwd = process.cwd()
 
 const getBabelConfig = async () => {
@@ -54,4 +55,57 @@ const compliePackages = async callback => {
   })
 }
 
-compliePackages()
+const watchCompliePackages = async () => {
+  const config = await getBabelConfig()
+  console.log(chalk.green('Build Watching....'))
+  chokidar
+    .watch(path.resolve(cwd, './packages/*/src/**.js'), {
+      ignoreInitial: true,
+      ignored: /(^|[/\\])\../,
+      persistent: true
+    })
+    .on('add', async (filename) => {
+      try {
+        const contents = await transformFileAsync(filename, config)
+        const newFilename = filename.replace(
+          /packages\/([^/]+)\/src/,
+          'packages/$1/lib'
+        )
+        await fs.outputFile(newFilename, contents.code)
+        console.log(chalk.green(`New File: ${filename}`))
+      } catch (e) {
+        console.error(e)
+      }
+    })
+    .on('change', async (filename) => {
+      try {
+        const contents = await transformFileAsync(filename, config)
+        const newFilename = filename.replace(
+          /packages\/([^/]+)\/src/,
+          'packages/$1/lib'
+        )
+        await fs.outputFile(newFilename, contents.code)
+        console.log(chalk.green(`File build success : ${filename}`))
+      } catch (e) {
+        console.error(e)
+      }
+    })
+    .on('unlink', async (filename) => {
+      try {
+        const newFilename = filename.replace(
+          /packages\/([^/]+)\/src/,
+          'packages/$1/lib'
+        )
+        await fs.remove(newFilename)
+        console.log(chalk.green(`Remove File: ${filename}`))
+      } catch (e) {
+        console.error(e)
+      }
+    })
+}
+
+if (process.argv.indexOf('--watch')) {
+  watchCompliePackages()
+} else {
+  compliePackages()
+}
