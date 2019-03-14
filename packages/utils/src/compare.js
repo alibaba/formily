9f5f093af0f83f9d43afc60198d18cbb582b11cb
janryWang <wangzhili56@126.com>
2019-03-14 10:28:10 +0800

    fix(@uform/core): fix buffer setFieldState is too slow

--- packages/utils/src/compare.js
+++ packages/utils/src/compare.js
@@ -32,7 +32,9 @@ function equal(a, b, filter) {
     var regexpB = b instanceof RegExp
     if (regexpA != regexpB) return false
     if (regexpA && regexpB) return a.toString() == b.toString()
-
+    var urlA = a instanceof URL
+    var urlB = b instanceof URL
+    if (urlA && urlB) return urlA.href == urlB.href
     var keys = keyList(a)
     length = keys.length
 
