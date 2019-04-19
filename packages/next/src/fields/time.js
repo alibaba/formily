e9659ac33b13d21de79960d3300ce55f4459246a
janryWang <wangzhili56@126.com>
2019-04-19 12:36:35 +0800

    fix(@uform/nexxt): fix time picker click will throw error

--- packages/next/src/fields/time.js
+++ packages/next/src/fields/time.js
@@ -2,10 +2,17 @@ import { connect, registerFormField } from '@uform/react'
 import { TimePicker } from '@alifd/next'
 import { mapStyledProps, mapTextComponent } from '../utils'
 
+const transformMoment = (value, format = 'YYYY-MM-DD HH:mm:ss') => {
+  return value && value.format ? value.format(format) : value
+}
+
 registerFormField(
   'time',
   connect({
     getProps: mapStyledProps,
+    getValueFromEvent(value) {
+      return transformMoment(value, 'HH:mm:ss')
+    },
     getComponent: mapTextComponent
   })(TimePicker)
 )
