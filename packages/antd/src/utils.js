a839faf46cedf45adebf58f4348b36fb36248bcf
janryWang <wangzhili56@126.com>
2019-06-17 11:41:25 +0800

    fix(@uform/antd): fix antd loading style

--- packages/antd/src/utils.js
+++ packages/antd/src/utils.js
@@ -26,7 +26,7 @@ const WrapSelect = styled(
   }
 )`
   min-width: 100px;
-  width:100%;
+  width: 100%;
 `
 
 const Text = styled(props => {
@@ -66,6 +66,8 @@ const Text = styled(props => {
     line-height: 40px;
   }
 `
+
+const loadingSvg = `<svg viewBox="0 0 1024 1024" class="anticon-spin" data-icon="loading" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false"><path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 0 0-94.3-139.9 437.71 437.71 0 0 0-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"></path></svg>`
 
 export const StateLoading = Target => {
   return class Select extends React.Component {
@@ -109,6 +111,10 @@ export const StateLoading = Target => {
               }
             }
           })
+          if (icon.innerHTML) {
+            icon.oldHTML = icon.innerHTML
+            icon.innerHTML = loadingSvg
+          }
           if (!icon.classList.contains(loadingName)) {
             icon.classList.add(loadingName)
           }
@@ -117,6 +123,7 @@ export const StateLoading = Target => {
           this.classList.forEach(className => {
             icon.classList.add(className)
           })
+          if (icon.oldHTML) icon.innerHTML = icon.oldHTML
           this.classList = []
         }
       }
