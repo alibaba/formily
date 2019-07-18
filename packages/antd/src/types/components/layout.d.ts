c2b764e292395d16e355f95e12f1cd8732efbee0
atzcl <atzcl0310@gmail.com>
2019-07-18 13:52:48 +0800

    refactor: modify FormLayout typings

--- packages/antd/src/types/components/layout.d.ts
+++ packages/antd/src/types/components/layout.d.ts
@@ -1,16 +1,16 @@
+import { IFieldProps } from '../../../../react/lib/type'
+
 export interface IFormLayoutProps {
-  className?: string
-  inline?: boolean
-  labelAlign?: 'left' | 'top' | 'inset'
-  wrapperCol?: number
-  labelCol?: number
-  labelTextAlign?: 'left' | 'right'
-  size?: 'small' | 'medium' | 'large'
-  style?: React.CSSProperties
-  children: React.ReactNode
+  className: string
+  inline: boolean
+  labelAlign: 'left' | 'top' | 'inset'
+  wrapperCol: number
+  labelCol: number
+  labelTextAlign: 'left' | 'right'
+  size: 'small' | 'medium' | 'large'
+  style: React.CSSProperties
+  name: string
+  render: (fieldProps: IFieldProps) => string | JSX.Element | null
 }
 
-export type TFormLayout = ({
-  children,
-  ...props
-}: IFormLayoutProps) => JSX.Element
+export type TFormLayout = React.FunctionComponent<Partial<IFormLayoutProps>>
