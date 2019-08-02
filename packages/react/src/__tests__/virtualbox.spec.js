4a6f4dc3dc45ac714ef6a39cf7fd0218730e2b75
Janry <wangzhili56@126.com>
2019-08-02 14:58:00 +0800

    fix(@uform/react): fix schema properties can not update when rerender (#220)

--- packages/react/src/__tests__/virtualbox.spec.js
+++ packages/react/src/__tests__/virtualbox.spec.js
@@ -33,6 +33,7 @@ registerFormField(
   connect()(props => <input {...props} value={props.value || ''} />)
 )
 registerFormField('text', connect()(props => <div>This is Text Component</div>))
+
 FormCard = createVirtualBox('card', ({ children }) => {
   return <div>card content{children}</div>
 })
@@ -87,4 +88,70 @@ test('dynamic node', async () => {
   fireEvent.click(queryByText('Change Editable'))
   await sleep(33)
   expect(queryByText('This is Text Component')).toBeVisible()
+})
+
+test('dynamic schema', async () => {
+  const TestComponent = () => {
+    const [schema, setSchema] = useState({
+      type: 'object',
+      properties: {
+        card: {
+          type: 'object',
+          'x-component': 'card',
+          properties: {
+            aa: {
+              type: 'string',
+              'x-props': {
+                'data-testid': 'aa'
+              }
+            },
+            bb: {
+              type: 'string',
+              'x-props': {
+                'data-testid': 'bb'
+              }
+            }
+          }
+        }
+      }
+    })
+
+    const deleteProperty = () =>
+      setSchema({
+        type: 'object',
+        properties: {
+          card: {
+            type: 'object',
+            'x-component': 'card',
+            properties: {
+              aa: {
+                type: 'string',
+                'x-props': {
+                  'data-testid': 'aa',
+                  disabled: true
+                }
+              }
+              // bb deleted
+            }
+          }
+        }
+      })
+    return (
+      <div>
+        <SchemaForm schema={schema} />
+        <button onClick={deleteProperty}>Delete</button>
+      </div>
+    )
+  }
+
+  const { queryByText, queryByTestId, baseElement } = render(<TestComponent />)
+
+  await sleep(33)
+  act(() => {
+    fireEvent.click(queryByText('Delete'))
+  })
+  await sleep(33)
+  expect(queryByTestId('aa').hasAttribute('disabled')).toBe(true)
+  expect(queryByTestId('aa')).toBeVisible()
+  expect(queryByTestId('bb')).toBeNull()
 })
