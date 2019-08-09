da36b86489237d64b7a9f08db185dbfd173c46c3
janrywang <wangzhili56@126.com>
2019-08-09 22:41:58 +0800

    fix(@uform/core): scheduler compact

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -13,8 +13,8 @@ export * from '@uform/utils'
 
 const self = globalThisPolyfill
 
-const bridgeScheduler = ([raf, caf, priority]) => {
-  return [callback => raf(priority, callback), caf]
+const compactScheduler = ([raf, caf, priority], compact: boolean) => {
+  return [compact ? callback => raf(priority, callback) : raf, caf]
 }
 
 const getScheduler = () => {
@@ -24,11 +24,14 @@ const getScheduler = () => {
   try {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const scheduler = require('scheduler')
-    return bridgeScheduler([
-      scheduler.scheduleCallback || scheduler.unstable_scheduleCallback,
-      scheduler.cancelCallback || scheduler.unstable_cancelCallback,
-      scheduler.NormalPriority || scheduler.unstable_NormalPriority
-    ])
+    return compactScheduler(
+      [
+        scheduler.scheduleCallback || scheduler.unstable_scheduleCallback,
+        scheduler.cancelCallback || scheduler.unstable_cancelCallback,
+        scheduler.NormalPriority || scheduler.unstable_NormalPriority
+      ],
+      !scheduler.unstable_forceFrameRate
+    )
   } catch (err) {
     return [self.requestAnimationFrame, self.cancelAnimationFrame]
   }
