a49b3c010743d51adecf8d4b1ddabc9d3c0c7ed0
janrywang <wangzhili56@126.com>
2019-08-09 23:32:25 +0800

    fix(@uform/builder): remove builder test case

--- packages/builder/src/__tests__/baseForm.spec.js
+++
@@ -1,8 +0,0 @@
-import * as Form from '../utils/baseForm'
-
-test('test baseForm', () => {
-  expect(Form).toHaveProperty('SchemaForm')
-  expect(Form).toHaveProperty('Field')
-  expect(Form).toHaveProperty('registerFormField')
-  expect(Form).toHaveProperty('registerFormFields')
-})
