4c647974969ef8fd91e871f0fcb593c3c23f8a13
atzcl <atzcl0310@gmail.com>
2019-08-26 22:21:25 +0800

    test: add SchemaForm triggers the processing of onSubmit

--- packages/react/src/__tests__/schema_form.spec.js
+++ packages/react/src/__tests__/schema_form.spec.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
-import SchemaForm, { Field, registerFormField, connect } from '../index'
+import SchemaForm, {
+  Field,
+  registerFormField,
+  connect,
+  createFormActions
+} from '../index'
 
 registerFormField(
   'string',
@@ -57,4 +62,127 @@ test('Increase lastValidateValue value processing during initialization', async 
   await sleep(1000)
   expect(inpueFieldValidate).toHaveBeenCalledTimes(1)
   expect(requriedFieldValidate).toHaveBeenCalledTimes(1)
+})
+
+const TestSubmitComponent = ({ children, ...props }) => (
+  <SchemaForm {...props}>
+    <Field
+      type="string"
+      x-rules={() =>
+        new Promise(resolve => {
+          // 延迟 1000ms 的异步校验
+          setTimeout(() => {
+            resolve()
+          }, 1000)
+        })
+      }
+    />
+    {children}
+  </SchemaForm>
+)
+
+test('test button submit type trigger onSubmit', async () => {
+  const handleSubmit = jest.fn()
+
+  const TestComponent = () => (
+    <TestSubmitComponent onSubmit={handleSubmit}>
+      <button type="submit" data-testid="submit">
+        submit
+      </button>
+    </TestSubmitComponent>
+  )
+
+  const { getByTestId } = render(<TestComponent />)
+
+  fireEvent.click(getByTestId('submit'))
+
+  await sleep(100)
+  fireEvent.click(getByTestId('submit'))
+  fireEvent.click(getByTestId('submit'))
+  fireEvent.click(getByTestId('submit'))
+
+  await sleep(1000)
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('test actions.submit trigger onSubmit', async () => {
+  const handleSubmit = jest.fn()
+
+  const actions = createFormActions()
+
+  const TestComponent = () => (
+    <TestSubmitComponent actions={actions} onSubmit={handleSubmit}>
+      <button
+        data-testid="submit"
+        onClick={() => {
+          actions.submit()
+        }}
+      >
+        submit
+      </button>
+    </TestSubmitComponent>
+  )
+
+  const { getByTestId } = render(<TestComponent />)
+
+  fireEvent.click(getByTestId('submit'))
+
+  await sleep(100)
+  fireEvent.click(getByTestId('submit'))
+  fireEvent.click(getByTestId('submit'))
+  fireEvent.click(getByTestId('submit'))
+
+  await sleep(1000)
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('test actions.submit and button submit type trigger onSubmit', async () => {
+  const handleSubmit = jest.fn()
+
+  const actions = createFormActions()
+
+  const TestComponent = () => (
+    <TestSubmitComponent
+      actions={actions}
+      onSubmit={() =>
+        new Promise(resolve => {
+          handleSubmit()
+          resolve()
+        })
+      }
+    >
+      <button type="submit" data-testid="htmlSubmit">
+        htmlSubmit
+      </button>
+      <button
+        data-testid="actionSubmit"
+        onClick={() => {
+          actions.submit()
+        }}
+      >
+        actionSubmit
+      </button>
+    </TestSubmitComponent>
+  )
+
+  const { getByTestId } = render(<TestComponent />)
+
+  fireEvent.click(getByTestId('htmlSubmit'))
+  await sleep(100)
+  fireEvent.click(getByTestId('actionSubmit'))
+  fireEvent.click(getByTestId('actionSubmit'))
+  fireEvent.click(getByTestId('actionSubmit'))
+
+  await sleep(1000)
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+
+  // actions.submit
+  fireEvent.click(getByTestId('actionSubmit'))
+  await sleep(100)
+  fireEvent.click(getByTestId('htmlSubmit'))
+  fireEvent.click(getByTestId('htmlSubmit'))
+  fireEvent.click(getByTestId('htmlSubmit'))
+
+  await sleep(1000)
+  expect(handleSubmit).toHaveBeenCalledTimes(2)
 })
