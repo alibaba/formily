d826e625606cbaf0fdf1bd2e0b832769ea94e5c2
janrywang <wangzhili56@126.com>
2019-09-09 11:22:01 +0800

    refactor(project): update build scripts

--- packages/antd/build.ts
+++ packages/antd/build.ts
@@ -4,39 +4,34 @@ import tsImportPluginFactory from 'ts-import-plugin'
 import glob from 'glob'
 
 const transformer = tsImportPluginFactory({
-  libraryName: 'antd'
-  //  style: 'css',
+  libraryName: 'antd',
+  //style: 'css',
 })
 
 function buildESM() {
-  const { fileNames, options } = getCompileConfig(
-    require.resolve('./tsconfig.json'),
-    {
-      outDir: './esm',
-      module: ts.ModuleKind.ESNext
-    }
-  )
-  compile(fileNames, options)
+  const { fileNames, options } = getCompileConfig(require.resolve('./tsconfig.json'), {
+    outDir: './esm',
+    module: ts.ModuleKind.ESNext
+  })
+  compile(fileNames, options, { before: [transformer] })
   console.log('esm build successfully')
 }
 
 function buildES5() {
   const rootNames = glob.sync('./esm/**/*.js')
-  compile(
-    rootNames,
-    {
-      allowJs: true,
-      esModuleInterop: true,
-      moduleResolution: ts.ModuleResolutionKind.NodeJs,
-      module: ts.ModuleKind.CommonJS,
-      target: ts.ScriptTarget.ES5,
-      outDir: './lib',
-      declaration: false
-    },
-    { before: [transformer] }
-  )
+  compile(rootNames, {
+    allowJs: true,
+    esModuleInterop: true,
+    moduleResolution: ts.ModuleResolutionKind.NodeJs,
+    module: ts.ModuleKind.CommonJS,
+    target: ts.ScriptTarget.ES5,
+    outDir: './lib',
+    declaration: false,
+  })
   console.log('es5 build successfully')
 }
+
+
 
 buildESM()
 buildES5()
