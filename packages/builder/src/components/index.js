7b6b2635cbaf1dc1f0c36130c0616a0a1515af91
Janry <wangzhili56@126.com>
2019-10-25 11:38:07 +0800

    Fix text component can not show JSX Element (#344)
    * refactor(pkg): update pkg
    
    * fix(@uform/next/antd): fix text can not show jsx Element
    
    * feat(@uform/builder): remove builder

--- packages/builder/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import PropsSetting from './props/propsSetting'
-import FieldList from './fields/index'
-import Preview from './preview/index'
-import GlobalBtnList from './globalBtnList/index'
-
-export { PropsSetting, FieldList, Preview, GlobalBtnList }
-
-export default {}
--- packages/builder/src/constants/itemType.js
+++
@@ -1,5 +0,0 @@
-export default {
-  CARD: 'card',
-  FIELD: 'field',
-  LAYOUT: 'layout'
-}
