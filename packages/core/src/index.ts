d4033111fc8c51c6d90db0381545f6d6c55b6aef
quirkyshop <quirkyvar@gmail.com>
2019-11-07 17:57:54 +0800

    feat: add silent option

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -769,9 +769,9 @@ export function createForm<FieldProps, VirtualFieldProps>(
     })
   }
 
-  function setFormState(callback?: (state: IFormState) => any) {
+  function setFormState(callback?: (state: IFormState) => any, silent?: boolean) {
     leadingUpdate(() => {
-      state.setState(callback)
+      state.setState(callback, silent)
     })
   }
 
@@ -800,13 +800,14 @@ export function createForm<FieldProps, VirtualFieldProps>(
 
   function setFieldState(
     path: FormPathPattern,
-    callback?: (state: IFieldState<FieldProps>) => void
+    callback?: (state: IFieldState<FieldProps>) => void,
+    silent?: boolean
   ) {
     if (!isFn(callback)) return
     let matchCount = 0
     let pattern = FormPath.getPath(path)
     graph.select(pattern, field => {
-      field.setState(callback)
+      field.setState(callback, silent)
       matchCount++
     })
     if (matchCount === 0 || pattern.isWildMatchPattern) {
@@ -828,10 +829,10 @@ export function createForm<FieldProps, VirtualFieldProps>(
     }
   }
 
-  function setFieldValue(path: FormPathPattern, value?: any) {
+  function setFieldValue(path: FormPathPattern, value?: any, silent?: boolean) {
     setFieldState(path, state => {
       state.value = value
-    })
+    }, silent)
   }
 
   function getFieldValue(path?: FormPathPattern) {
@@ -840,10 +841,10 @@ export function createForm<FieldProps, VirtualFieldProps>(
     })
   }
 
-  function setFieldInitialValue(path?: FormPathPattern, value?: any) {
+  function setFieldInitialValue(path?: FormPathPattern, value?: any, silent?: boolean) {
     setFieldState(path, state => {
       state.initialValue = value
-    })
+    }, silent)
   }
 
   function getFieldInitialValue(path?: FormPathPattern) {
