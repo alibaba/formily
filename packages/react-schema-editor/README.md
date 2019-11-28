bf144780969285a6e6a598c0414d602731e4f589
黄子毅 <576625322@qq.com>
2019-11-28 11:57:27 +0800

    表单 editor 新增菜单树功能 (#441)
    * feat: merge master
    
    * feat: editor 新增菜单树
    
    * feat: fix bug

--- packages/react-schema-editor/README.md
+++ packages/react-schema-editor/README.md
@@ -6,5 +6,49 @@
 import React from 'react'
 import { SchemaEditor } from './src'
 
-ReactDOM.render(<SchemaEditor />, document.getElementById('root'))
+function SchemaEditorDemo() {
+  const [schema, setSchema] = React.useState({
+      type: 'object',
+      title: '我是表单标题',
+      description: '我是表单描述',
+      properties: {
+        fieldA: {
+          type: 'object',
+          title: '我是层级嵌套标题',
+          description: '我是层级嵌套描述',
+          properties: {
+            arrayA: {
+              type: 'array',
+              title: '动物',
+              description: '我是字段描述',
+              component: 'Select',
+              items: {
+                type: 'string',
+                enum: ['Dog', 'Cat', 'Horse']
+              }
+            },
+            numberB: {
+              type: 'number',
+              title: '年龄'
+            },
+            objectC: {
+              type: 'object'
+            }
+          }
+        }
+      }
+    })
+
+  return (
+    <SchemaEditor
+      schema={schema}
+      onChange={setSchema}
+    />
+  )
+}
+
+ReactDOM.render(
+  <SchemaEditorDemo />,
+  document.getElementById('root')
+)
 ```
--- packages/react-schema-editor/src/index.tsx
+++ packages/react-schema-editor/src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Button, Icon, Grid } from '@alifd/next'
-import { jsonToSchema } from './utils'
+import { Button, Icon, Row, Col } from 'antd'
 import { SchemaTree } from './components/SchemaTree'
 import { SchemaCode } from './components/SchemaCode'
-import json from './utils/schema'
-const { Row, Col } = Grid
 
-export const SchemaEditor: React.FC = () => {
-  const initialSchema = jsonToSchema(json)
-  const [schema, setSchema] = React.useState(initialSchema)
+import 'antd/dist/antd.css'
 
+export const SchemaEditor: React.FC<{
+  schema: any
+  onChange: (schema: any) => void
+}> = ({ schema, onChange }) => {
   return (
     <div className="schema-editor">
       <div className="schema-menus">
@@ -20,10 +19,10 @@ export const SchemaEditor: React.FC = () => {
       </div>
       <Row className="schema-editor-main">
         <Col span={14} className="schema-col schema-tree splitter">
-          <SchemaTree schema={schema} onChange={setSchema}></SchemaTree>
+          <SchemaTree schema={schema} onChange={onChange} />
         </Col>
         <Col span={10} className="schema-col schema-code">
-          <SchemaCode schema={schema} onChange={setSchema}></SchemaCode>
+          <SchemaCode schema={schema} onChange={onChange}></SchemaCode>
         </Col>
       </Row>
     </div>
