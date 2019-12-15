1a724947a1a77652c57158ca949bee53a17447e2
Janry <wangzhili56@126.com>
2019-12-15 13:46:26 +0800

    Fix destructor (#506)

--- packages/react-schema-renderer/src/shared/linkage.ts
+++ packages/react-schema-renderer/src/shared/linkage.ts
@@ -1,8 +1,40 @@
 import { JSONCondition } from '../shared/condition'
 import { FormPath, IFormEffect } from '@uform/react'
-import { isFn } from '@uform/shared'
+import { isFn, isStr } from '@uform/shared'
 import { ISchemaFormActions } from '../types'
 import { compileObject } from './expression'
+
+const pathExpRE = /\[\s*(?:([+-])\s*(\d+)?)?\s*\]/g
+
+const transformTargetPath = (target: string, indexes: number[]) => {
+  if (!isStr(target)) return target
+  let index = 0
+  const newTarget = target.replace(
+    pathExpRE,
+    (_: string, operator: string, delta: string) => {
+      if (delta) {
+        if (operator === '+') {
+          return String(indexes[index++] + Number(delta))
+        } else if (operator === '-') {
+          return String(indexes[index++] - Number(delta))
+        }
+      } else {
+        if (operator === '+') {
+          return String(indexes[index++] + 1)
+        } else if (operator === '-') {
+          return String(indexes[index++] - 1)
+        }
+      }
+      return String(indexes[index++])
+    }
+  )
+  pathExpRE.lastIndex = 0
+  return newTarget
+}
+
+const getPathIndexes = (path: any): number[] => {
+  return path.transform(/\d+/, (...args) => args.map(i => Number(i))) as any
+}
 
 export const presetLinkage = (
   resolve: (params: any, actions: ISchemaFormActions) => void,
@@ -12,9 +44,12 @@ export const presetLinkage = (
     const { name, target, triggerType = 'onFieldChange' } = params
     const { hasChanged, getFormState, getFieldState } = actions
     $(triggerType, name).subscribe(fieldState => {
+      const fieldName = FormPath.parse(fieldState.name)
+      const fieldIndexes = getPathIndexes(fieldName)
       const formState = getFormState()
-      const targetState = getFieldState(target)
-      const fieldValue = FormPath.parse(name).getIn(formState.values)
+      const newTarget = transformTargetPath(target, fieldIndexes)
+      const targetState = getFieldState(newTarget)
+      const fieldValue = fieldName.getIn(formState.values)
       const options = compileObject(params, {
         ...context,
         $value: fieldValue,
@@ -23,6 +58,7 @@ export const presetLinkage = (
         $target: targetState || {}
       })
       options.condition = JSONCondition.calculate(options.condition, fieldValue)
+      options.target = newTarget
       if (
         hasChanged(fieldState, 'value') ||
         hasChanged(fieldState, 'initialized') ||
