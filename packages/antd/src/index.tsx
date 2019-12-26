862f0a9c1472ab6a3f9a6c22a03b636040fb36a3
Janry <wangzhili56@126.com>
2019-12-26 12:05:07 +0800

    Export form item components (#547)

--- packages/antd/src/index.tsx
+++ packages/antd/src/index.tsx
@@ -5,7 +5,7 @@ import {
 } from '@uform/react-schema-renderer'
 import { IAntdSchemaFormProps, IAntdSchemaFieldProps } from './types'
 import './fields'
-import './compat'
+export * from './compat'
 export * from '@uform/react-schema-renderer'
 export * from './components'
 export * from './types'
