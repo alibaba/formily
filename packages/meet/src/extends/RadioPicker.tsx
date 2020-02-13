d1cf8b4cf984684aa9f2c57eaa464891eba64fbb
DarK-AleX-Alibaba <860287221@qq.com>
2020-02-13 21:46:30 +0800

    feat: add newLine

--- packages/meet/src/extends/RadioPicker.tsx
+++ packages/meet/src/extends/RadioPicker.tsx
@@ -9,78 +9,78 @@ const { Group: RadioGroup } = Radio;
 const { Group: CheckboxGroup } = Checkbox;
 
 const RadioPicker = (props, ref) => {
-    const {
-        type = SelectType.SINGLE,
-        data = [],
-        visible,
-        onHide,
-        title,
-        styles: stylesProp,
-        onChange,
-        value: valueProp,
-        view = false,
-        ...others
-    } = props;
-    const [ preValue, setPreValue ] = useState(null);
-    const [ value, setValue ] = useState(null);
-    const local = useLocale('Select', others, {
-        ok: '确定',
-        cancel: '取消'
-    })
+  const {
+    type = SelectType.SINGLE,
+    data = [],
+    visible,
+    onHide,
+    title,
+    styles: stylesProp,
+    onChange,
+    value: valueProp,
+    view = false,
+    ...others
+  } = props;
+  const [preValue, setPreValue] = useState(null);
+  const [value, setValue] = useState(null);
+  const local = useLocale('Select', others, {
+    ok: '确定',
+    cancel: '取消'
+  })
 
-    if (preValue !== valueProp) {
-        setPreValue(valueProp);
-        setValue(valueProp);
-    }
+  if (preValue !== valueProp) {
+    setPreValue(valueProp);
+    setValue(valueProp);
+  }
 
-    const styles = useStyles({}, {
-        styles: stylesProp
-    }, (classNames) => ({
-        picker: classNames('select__picker'),
-        toolbar: classNames('select__picker__toolbar'),
-        title: classNames('select__picker__title'),
-        titleText: classNames('select__picker__title__text'),
-        content: classNames('select__picker__content')
-    }));
+  const styles = useStyles({}, {
+    styles: stylesProp
+  }, (classNames) => ({
+    picker: classNames('select__picker'),
+    toolbar: classNames('select__picker__toolbar'),
+    title: classNames('select__picker__title'),
+    titleText: classNames('select__picker__title__text'),
+    content: classNames('select__picker__content')
+  }));
 
-    const handleChange = (v) => {
-        // console.log(v)
-        setValue(v);
-    }
-    
-    const handleSure = () => {
-        onHide();
-        onChange(value);
-    };
+  const handleChange = (v) => {
+    // console.log(v)
+    setValue(v);
+  }
 
-    const Group = type === SelectType.SINGLE ? RadioGroup : CheckboxGroup;
-    let pickerValue = value;
-    if(type === SelectType.MULTIPLE) {
-        pickerValue = value ? Array.isArray(value) ? value : [value] : []
-    }
+  const handleSure = () => {
+    onHide();
+    onChange(value);
+  };
 
-    return (
-        <Drawer
-        placement="bottom"
-        view={view}
-        styles={stylesProp}
-        visible={visible}
-        onMaskClick={onHide}>
-        <RaxView ref={ref}>
-            <Group
-            data={data}
-            value={pickerValue}
-            styles={styles}
-            onChange={handleChange}/>
-            <Button
-            styles={stylesProp}
-            onClick={handleSure}
-            type="primary">
-                {local.ok}
-            </Button>
-        </RaxView>
-        </Drawer>
-    )
+  const Group = type === SelectType.SINGLE ? RadioGroup : CheckboxGroup;
+  let pickerValue = value;
+  if (type === SelectType.MULTIPLE) {
+    pickerValue = value ? Array.isArray(value) ? value : [value] : []
+  }
+
+  return (
+    <Drawer
+      placement="bottom"
+      view={view}
+      styles={stylesProp}
+      visible={visible}
+      onMaskClick={onHide}>
+      <RaxView ref={ref}>
+        <Group
+          data={data}
+          value={pickerValue}
+          styles={styles}
+          onChange={handleChange} />
+        <Button
+          styles={stylesProp}
+          onClick={handleSure}
+          type="primary">
+          {local.ok}
+        </Button>
+      </RaxView>
+    </Drawer>
+  )
 }
 
 export default forwardRef(RadioPicker)
--- packages/meet/src/extends/RadioSelect.tsx
+++ packages/meet/src/extends/RadioSelect.tsx
@@ -8,71 +8,71 @@ import ButtonStylesProvider from '@alifd/meet/lib/button/styles';
 import SelectStylesProvider from '@alifd/meet/lib/select/styles';
 const { Base } = Select;
 const getLabel = (value, data, delimiter) => {
-    const pickerValue = value ? Array.isArray(value) ? value : [value] : [];
-    if (!pickerValue || pickerValue.length === 0) {
-        return '';
-    }
-    const treeChildren = pickerValue.map((v) => data.find((d) => d.value === v));
-    return treeChildren.map((v) => v ? v.label : '').join(delimiter);
+  const pickerValue = value ? Array.isArray(value) ? value : [value] : [];
+  if (!pickerValue || pickerValue.length === 0) {
+    return '';
+  }
+  const treeChildren = pickerValue.map((v) => data.find((d) => d.value === v));
+  return treeChildren.map((v) => v ? v.label : '').join(delimiter);
 }
 
 const RadioSelect = React.forwardRef((props, ref) => {
+  const {
+    type = 'single',
+    delimiter = ',',
+    title,
+    locale,
+    styles = {},
+    ...others
+  } = props;
+  const renderPicker = (_ref) => {
     const {
-        type = 'single',
-        delimiter = ',',
-        title,
-        locale,
-        styles = {},
-        ...others
-    } = props;
-    const renderPicker = (_ref) => {
-        const {
-            data,
-            value,
-            visible,
-            onHide,
-            onChange,
-            valueLabel,
-            setValueLabel
-        } = _ref;
-        const label = getLabel(value, data, delimiter);
-    
-        if (label !== valueLabel) {
-            setValueLabel(label);
-        }
-        // const onChange = (v) => {
-        //     console.log(v);
-        //     return _onChange(type === SelectType.SINGLE ? v[0] : v);
-        // }
-    
-        return (
-            <RadioPicker 
-            {...others}
-            type={type}
-            data={data}
-            visible={visible}
-            onHide={onHide}
-            title={title}
-            styles={styles}
-            onChange={onChange}
-            value={value}/>
-        )
+      data,
+      value,
+      visible,
+      onHide,
+      onChange,
+      valueLabel,
+      setValueLabel
+    } = _ref;
+    const label = getLabel(value, data, delimiter);
+
+    if (label !== valueLabel) {
+      setValueLabel(label);
     }
+    // const onChange = (v) => {
+    //     console.log(v);
+    //     return _onChange(type === SelectType.SINGLE ? v[0] : v);
+    // }
 
     return (
-    <Base 
+      <RadioPicker
         {...others}
+        type={type}
+        data={data}
+        visible={visible}
+        onHide={onHide}
+        title={title}
         styles={styles}
-        renderPicker={renderPicker}
-        ref={ref}
+        onChange={onChange}
+        value={value} />
+    )
+  }
+
+  return (
+    <Base
+      {...others}
+      styles={styles}
+      renderPicker={renderPicker}
+      ref={ref}
     />)
 })
 
 const StyledSelect = WithStyles(RadioSelect, [
-    DrawerStylesProvider,
-    TextFieldStylesProvider,
-    ButtonStylesProvider,
-    SelectStylesProvider,
+  DrawerStylesProvider,
+  TextFieldStylesProvider,
+  ButtonStylesProvider,
+  SelectStylesProvider,
 ]);
 
 export default StyledSelect;
