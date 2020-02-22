9331f2e9279156392c2e7701fa22db44801b66d7
锦此 <jinc.cjc@alibaba-inc.com>
2020-02-22 14:11:08 +0800

    Feat no component tips (#662)

--- packages/next/src/components/FormItem.tsx
+++ packages/next/src/components/FormItem.tsx
@@ -66,7 +66,8 @@ export const FormItem: React.FC<INextFormItemProps> = topProps => {
   const renderComponent = ({ props, state, mutators, form }) => {
     if (!component) {
       if (children) return <Fragment>{children}</Fragment>
-      return <div>Can not fount component.</div>
+      console.error(`[Formily Error]: Can't fount the component. Its key is ${name}.`)
+      return null
     }
     if (!component['__ALREADY_CONNECTED__']) {
       component[ConnectedComponent] = connect({
