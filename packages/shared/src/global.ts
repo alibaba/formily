4d0478603074ee84b90434e0863f8f22110d47bb
Neil Guo <handsamechina@126.com>
2020-02-24 22:09:15 +0800

    perf(global): prevent causing CSP

--- packages/shared/src/global.ts
+++ packages/shared/src/global.ts
@@ -1,19 +1,13 @@
-function globalSelf() {
-  try {
-    if (typeof self !== 'undefined') {
-      return self
-    }
-  } catch (e) {}
-  try {
-    if (typeof window !== 'undefined') {
-      return window
-    }
-  } catch (e) {}
-  try {
-    if (typeof global !== 'undefined') {
-      return global
-    }
-  } catch (e) {}
-  return Function('return this')()
-}
-export const globalThisPolyfill = globalSelf()
+// reference https://mathiasbynens.be/notes/globalthis
+;(function(Object) {
+  if (typeof globalThis === 'object') return
+  Object.defineProperty(Object.prototype, '__magic__', {
+    get: function() {
+      return this
+    },
+    configurable: true // This makes it possible to `delete` the getter later.
+  })
+  __magic__.globalThis = __magic__ // lolwat
+  delete Object.prototype.__magic__
+})(Object)
+export const globalThisPolyfill = globalThis
