095fca519193f63fd0f150e46a5525f211448761
Neil Guo <handsamechina@126.com>
2020-02-24 22:22:45 +0800

    fix(global): disable typescript checking

--- packages/shared/src/global.ts
+++ packages/shared/src/global.ts
@@ -1,13 +1,14 @@
 // reference https://mathiasbynens.be/notes/globalthis
+// @ts-nocheck
 ;(function(Object) {
   if (typeof globalThis === 'object') return
   Object.defineProperty(Object.prototype, '__magic__', {
     get: function() {
       return this
     },
-    configurable: true // This makes it possible to `delete` the getter later.
+    configurable: true
   })
-  __magic__.globalThis = __magic__ // lolwat
+  __magic__.globalThis = __magic__ 
   delete Object.prototype.__magic__
 })(Object)
 export const globalThisPolyfill = globalThis
