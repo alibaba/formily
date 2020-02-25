5af489311c5adb644941981e92d9923cdde975ae
Neil <handsamechina@126.com>
2020-02-25 11:45:30 +0800

    perf(array): shorten the code (#678)

--- packages/shared/src/array.ts
+++ packages/shared/src/array.ts
@@ -31,8 +31,7 @@ type MemoObjectIterator<TValue, TResult> = (
   key: string
 ) => TResult
 
-export const toArr = (val: any): any[] => (isArr(val) ? val : val ? [val] : [])
-
+export const toArr = (val: any): any[] => [].concat(val || [])
 export function each(
   val: string,
   iterator: EachStringIterator,
