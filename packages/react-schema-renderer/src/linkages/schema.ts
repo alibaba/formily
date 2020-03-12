63121e40bd48b491acaad4b67e53bf7cbe5ffd2c
Janry <wangzhili56@126.com>
2020-03-12 10:17:34 +0800

    fix(schema-renderer): fix linkages (#713)

--- packages/react-schema-renderer/src/linkages/schema.ts
+++ packages/react-schema-renderer/src/linkages/schema.ts
@@ -3,15 +3,31 @@ import { merge } from '@formily/shared'
 export const useValueSchemaLinkageEffect = (scope?: any) =>
   useValueLinkageEffect({
     type: 'value:schema',
-    resolve: ({ target, schema }, { setFieldState }) => {
+    resolve: ({ target, complie }, { setFieldState }) => {
       setFieldState(target, innerState => {
-        merge(innerState.props, schema)
+        merge(
+          innerState.props,
+          complie('schema', {
+            $target: innerState
+          }),
+          {
+            assign: true
+          }
+        )
       })
     },
-    reject: ({ target, otherwise }, { setFieldState }) => {
+    reject: ({ target, otherwise, complie }, { setFieldState }) => {
       if (!otherwise) return
       setFieldState(target, innerState => {
-        merge(innerState.props, otherwise)
+        merge(
+          innerState.props,
+          complie('otherwise', {
+            $target: innerState
+          }),
+          {
+            assign: true
+          }
+        )
       })
     },
     scope
