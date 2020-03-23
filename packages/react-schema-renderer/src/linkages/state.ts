159e533a9bda03643d1169d9c44e3c26d8a0034d
janryWang <wangzhili56@126.com>
2020-03-23 16:26:51 +0800

    fix(schema-renderer): fix array merge

--- packages/react-schema-renderer/src/linkages/state.ts
+++ packages/react-schema-renderer/src/linkages/state.ts
@@ -11,7 +11,8 @@ export const useValueStateLinkageEffect = (scope?: any) =>
             $target: innerState
           }),
           {
-            assign: true
+            assign: true,
+            arrayMerge: (target, source) => source
           }
         )
       })
@@ -25,7 +26,8 @@ export const useValueStateLinkageEffect = (scope?: any) =>
             $target: innerState
           }),
           {
-            assign: true
+            assign: true,
+            arrayMerge: (target, source) => source
           }
         )
       })
