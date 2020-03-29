c3c15efc168c123619dc305e74b9dfa73bce3375
林法鑫 <linlinfaxin@163.com>
2020-03-30 01:58:52 +0800

    fix(share): fix toArr

--- packages/shared/src/array.ts
+++ packages/shared/src/array.ts
@@ -31,7 +31,7 @@ type MemoObjectIterator<TValue, TResult> = (
   key: string
 ) => TResult
 
-export const toArr = (val: any): any[] => isArr(val) ? val : (val !== undefined ? [val] : [])
+export const toArr = (val: any): any[] => isArr(val) ? val : (val ? [val] : [])
 export function each(
   val: string,
   iterator: EachStringIterator,
