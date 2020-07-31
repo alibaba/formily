506c44f0ba13df63c5bf6baaf6c259753746167a
soulwu <soulwuzjjx@vip.qq.com>
2020-07-31 14:53:16 +0800

    fix: rollback the behavior of mutators.move and add mutators.swap

--- packages/core/src/externals.ts
+++ packages/core/src/externals.ts
@@ -1064,12 +1064,20 @@ export const createFormExternals = (
       shift() {
         return mutators.remove(0)
       },
-      move($from: number, $to: number) {
+      swap() {
         const arr = toArr(getValue()).slice()
         const fromItem = arr[$from]
         const toItem = arr[$to]
         arr[$from] = toItem
         arr[$to] = fromItem
+        setValue(arr)
+        return arr
+      },
+      move($from: number, $to: number) {
+        const arr = toArr(getValue()).slice()
+        const item = arr[$from]
+        arr.splice($from, 1)
+        arr.splice($to, 0, item)
         setValue(arr)
         return arr
       },
