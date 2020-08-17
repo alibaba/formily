fd9b5221719ccbc159de1dbcd9bc6d67e2a3e243
janrywang <wangzhili56@126.com>
2020-08-17 13:14:43 +0800

    fix(react): fix ci

--- packages/react/src/hooks/useForceUpdate.ts
+++ packages/react/src/hooks/useForceUpdate.ts
@@ -1,14 +1,26 @@
-import { useState, useRef, useEffect } from 'react'
+import { useCallback, useState, useEffect, useRef } from 'react'
+
+// Returning a new object reference guarantees that a before-and-after
+//   equivalence check will always be false, resulting in a re-render, even
+//   when multiple calls to forceUpdate are batched.
 
 export function useForceUpdate(): () => void {
-  const updating = useRef(true)
-  const [, setState] = useState(Object.create(null))
+  const updating = useRef(false)
+  const timer = useRef(null)
+  const [, dispatch] = useState<{}>(Object.create(null))
+  updating.current = true
+  // Turn dispatch(required_parameter) into dispatch().
+  const memoizedDispatch = useCallback((): void => {
+    if (!updating.current) dispatch(Object.create(null))
+    else {
+      clearTimeout(timer.current)
+      timer.current = setTimeout(()=>{
+        dispatch(Object.create(null))
+      })
+    }
+  }, [dispatch])
   useEffect(() => {
     updating.current = false
   })
-  updating.current = true
-  return () => {
-    if (updating.current) return
-    setState(Object.create(null))
-  }
+  return memoizedDispatch
 }
