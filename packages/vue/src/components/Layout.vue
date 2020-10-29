315ba3bffd07daae48a13e3a03c43787da5ad91b
MisicDemone <424532913@qq.com>
2020-10-29 14:54:56 +0800

    feat(vue): add props of layout components

--- packages/vue/src/components/Layout.vue
+++ packages/vue/src/components/Layout.vue
@@ -11,7 +11,42 @@ import { ILayoutProps } from '../types'
 import { useLayout } from '../hooks/useLayout'
 
 export default defineComponent({
-  props: {},
+  props: {
+    /* eslint-disable vue/require-prop-types  */
+    /* eslint-disable vue/require-default-prop */
+    hasBorder: Boolean,
+    context: {},
+    isRoot: Boolean,
+    isLayout: Boolean,
+    defaultSettings: {},
+    children: {},
+    full: Boolean,
+    layoutProps: {},
+    className: String,
+    label: {},
+    required: Boolean,
+    labelAlign: String,
+    inline: Boolean,
+    inset: Boolean,
+    autoRow: Boolean,
+    columns: Number,
+    flex: Boolean,
+    enableSafeWidth: Boolean,
+    labelWidth: [Number, String],
+    wrapperWidth: [Number, String],
+    labelCol: Number,
+    wrapperCol: Number,
+    addonBefore: {},
+    addonAfter: {},
+    description: {},
+    gutter: [Number, String],
+    span: Number,
+    grid: Boolean,
+    contextColumns: Number,
+    contextResponsive: Object,
+    responsive: Object,
+    size: String
+  },
   setup({
     required,
     label,
--- packages/vue/src/components/LayoutItem.vue
+++ packages/vue/src/components/LayoutItem.vue
@@ -9,6 +9,18 @@ import { defineComponent } from '@vue/composition-api'
 import { useLayoutItem } from '../hooks/useLayout'
 
 export default defineComponent({
+  props: {
+    /* eslint-disable vue/require-prop-types  */
+    /* eslint-disable vue/require-default-prop */
+    span: Number,
+    full: Boolean,
+    labelAlign: String,
+    inset: Boolean,
+    labelWidth: Number,
+    wrapperWidth: Number,
+    labelCol: Number,
+    wrapperCol: Number
+  },
   setup(props) {
     const layoutItem = useLayoutItem(props)
     return {
