6c84eabab1ad0330431d58c0700e2648dbba29a0
janrywang <wangzhili56@126.com>
2020-11-23 23:41:18 +0800

    feat(core): update typings

--- packages/react/src/components/Form.tsx
+++ packages/react/src/components/Form.tsx
@@ -3,9 +3,13 @@ import { FormContext } from '../context'
 import { useForm } from '../hooks'
 import { IFormProps } from '../types'
 
-export const Form: React.FC<IFormProps> = props => {
+export function Form<
+  Component extends
+    | keyof JSX.IntrinsicElements
+    | React.JSXElementConstructor<any>
+>(props: React.PropsWithChildren<IFormProps<Component>>) {
   const form = useForm(props)
-  const Form = props.component
+  const Form = props.component as 'form'
   return (
     <FormContext.Provider value={form}>
       <Form
--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -1,14 +1,17 @@
 import React from 'react'
 
-export interface IFormProps
-  extends Omit<Formily.ICreateFormOptions, 'values' | 'initialValues'> {
+type IReactComponent =
+  | keyof JSX.IntrinsicElements
+  | React.JSXElementConstructor<any>
+
+export type IFormProps<Component extends IReactComponent> = Omit<
+  Formily.ICreateFormOptions,
+  'values' | 'initialValues'
+> & {
   form?: Formily.Form
   value?: any
   initialValues?: any
   onChange?: (values: any) => void
   onSubmit?: (values: any) => Promise<any> | void
-  component?:
-    | string
-    | React.FunctionComponent<React.HTMLAttributes<HTMLFormElement>>
-    | React.ComponentClass<React.HTMLAttributes<HTMLFormElement>>
-}
+  component?: Component
+} & Omit<React.ComponentProps<Component>, 'component' | 'onSubmit'>
