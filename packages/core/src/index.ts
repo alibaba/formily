bc61cd665c6c26085e16af47e8d6093d3e4d4a63
janrywang <wangzhili56@126.com>
2020-11-27 14:18:41 +0800

    feat(react): improve code

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,8 @@
 import { ICreateFormOptions } from './types'
 import { Form } from './models'
 export * from './namespace'
+export * from './effects'
 export * from './effect'
-export * from './hooks'
 export const createForm = (options: ICreateFormOptions) => {
   return new Form(options)
 }
