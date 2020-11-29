92ff50513d4c28e7406742f13cf20cbb7db1200a
janrywang <wangzhili56@126.com>
2020-11-29 23:49:08 +0800

    feat(react): improve code

---
+++ packages/react/src/global.ts
@@ -0,0 +1,7 @@
+import * as Types from './types'
+
+declare global {
+  namespace Formily.React {
+    export { Types }
+  }
+}
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
 export * from '@formily/core'
 export * from './components'
+export * from './global'
 export * from './hooks'
