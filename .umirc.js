e172da6d0810a64bdfc734fd62c4ad7d2a0759a5
janrywang <wangzhili56@126.com>
2021-01-08 22:44:06 +0800

    docs(project): update docs

--- .umirc.js
+++ .umirc.js
@@ -1,11 +1,17 @@
 export default {
   mode: 'site',
-  logo: '//img.alicdn.com/tfs/TB1La8Uu7Y2gK0jSZFgXXc5OFXa-372-194.png',
+  logo:
+    '//img.alicdn.com/imgextra/i2/O1CN01f6Rq6L1IRCkxNn447_!!6000000000889-55-tps-600-89.svg',
+  title: 'Formily',
+  hash: true,
+  favicon:
+    '//img.alicdn.com/imgextra/i3/O1CN01XtT3Tv1Wd1b5hNVKy_!!6000000002810-55-tps-360-360.svg',
+  outputPath: './doc-site',
   title: 'Formily',
   navs: [
     {
       title: '指南',
-      path: '/guide'
+      path: '/guide',
     },
     {
       title: 'API手册',
@@ -13,17 +19,17 @@ export default {
       children: [
         {
           title: 'Core API',
-          path: '/api/core'
+          path: '/api/core',
         },
         {
           title: 'React API',
-          path: '/api/react'
+          path: '/api/react',
         },
         {
           title: 'React Schema API',
-          path: '/api/react-schema'
-        }
-      ]
+          path: '/api/react-schema',
+        },
+      ],
     },
     {
       title: '组件库',
@@ -31,31 +37,31 @@ export default {
       children: [
         {
           title: 'Antd Design',
-          path: '//antd.formilyjs.org'
+          path: '//antd.formilyjs.org',
         },
         {
           title: 'Alibaba Fusion',
-          path: '//fusion.formilyjs.org'
-        }
-      ]
+          path: '//fusion.formilyjs.org',
+        },
+      ],
     },
     {
       title: '论坛',
-      path: '//github.com/alibaba/formily/discussions'
+      path: '//github.com/alibaba/formily/discussions',
     },
     {
       title: '表单编辑器',
-      path: '//github.com/alibaba/formily-editor'
+      path: '//github.com/alibaba/formily-editor',
     },
     {
       title: 'Chrome扩展',
       path:
-        '//chrome.google.com/webstore/detail/formily-devtools/kkocalmbfnplecdmbadaapgapdioecfm?hl=zh-CN'
+        '//chrome.google.com/webstore/detail/formily-devtools/kkocalmbfnplecdmbadaapgapdioecfm?hl=zh-CN',
     },
     {
       title: 'GITHUB',
-      path: '//github.com/alibaba/formily'
-    }
+      path: '//github.com/alibaba/formily',
+    },
   ],
   styles: [
     `.__dumi-default-navbar-logo{
@@ -67,13 +73,13 @@ export default {
     nav a{
       text-decoration: none !important;
     }
-    `
+    `,
   ],
   menus: {
     '/guide': [
       {
         title: '介绍',
-        path: '/guide'
+        path: '/guide',
       },
       {
         title: '概念',
@@ -81,25 +87,25 @@ export default {
         children: [
           {
             title: 'MVVM',
-            path: '/guide/concept/mvvm'
+            path: '/guide/concept/mvvm',
           },
           {
             title: '充血模型',
-            path: '/guide/concept/model'
+            path: '/guide/concept/model',
           },
           {
             title: '生命周期',
-            path: '/guide/concept/lifecycle'
+            path: '/guide/concept/lifecycle',
           },
           {
             title: '路径系统',
-            path: '/guide/concept/path'
+            path: '/guide/concept/path',
           },
           {
             title: 'JSON Schema',
-            path: '/guide/concept/json-schema'
-          }
-        ]
+            path: '/guide/concept/json-schema',
+          },
+        ],
       },
       {
         title: '开发实践',
@@ -107,46 +113,46 @@ export default {
         children: [
           {
             title: '纯JSX驱动开发',
-            path: '/guide/practice/jsx-develop'
+            path: '/guide/practice/jsx-develop',
           },
           {
             title: 'JSON Schema驱动开发',
-            path: '/guide/practice/schema-develop'
+            path: '/guide/practice/schema-develop',
           },
           {
             title: 'JSX Schema驱动开发',
-            path: '/guide/practice/jsx-schema-develop'
+            path: '/guide/practice/jsx-schema-develop',
           },
           {
             title: '混合开发',
-            path: '/guide/practice/mixin-develop'
+            path: '/guide/practice/mixin-develop',
           },
           {
             title: '实现表单校验',
-            path: '/guide/practice/validations'
+            path: '/guide/practice/validations',
           },
           {
             title: '实现异步数据源',
-            path: '/guide/practice/asyncs'
+            path: '/guide/practice/asyncs',
           },
           {
             title: '实现联动逻辑',
-            path: '/guide/practice/linkages'
+            path: '/guide/practice/linkages',
           },
           {
             title: '实现联动计算器',
-            path: '/guide/practice/caculator'
+            path: '/guide/practice/caculator',
           },
           {
             title: '实现自定义组件',
-            path: '/guide/practice/custom-component'
+            path: '/guide/practice/custom-component',
           },
           {
             title: '管理业务逻辑',
-            path: '/guide/practice/best-practice'
-          }
-        ]
-      }
+            path: '/guide/practice/best-practice',
+          },
+        ],
+      },
     ],
     '/api': [
       {
@@ -155,21 +161,21 @@ export default {
         children: [
           {
             title: 'Package',
-            path: '/api/core'
+            path: '/api/core',
           },
           {
             title: 'Models',
-            path: '/api/core/models'
+            path: '/api/core/models',
           },
           {
             title: 'TypeChecker',
-            path: '/api/core/checker'
+            path: '/api/core/checker',
           },
           {
             title: 'Effects',
-            path: '/api/core/effects'
-          }
-        ]
+            path: '/api/core/effects',
+          },
+        ],
       },
       {
         title: 'React API',
@@ -177,17 +183,17 @@ export default {
         children: [
           {
             title: 'Package',
-            path: '/api/react'
+            path: '/api/react',
           },
           {
             title: 'Components',
-            path: '/api/react/components'
+            path: '/api/react/components',
           },
           {
             title: 'React Hooks',
-            path: '/api/react/hooks'
-          }
-        ]
+            path: '/api/react/hooks',
+          },
+        ],
       },
       {
         title: 'React Schema API',
@@ -195,17 +201,17 @@ export default {
         children: [
           {
             title: 'Package',
-            path: '/api/react-schema'
+            path: '/api/react-schema',
           },
           {
             title: 'Components',
-            path: '/api/react-schema/components'
+            path: '/api/react-schema/components',
           },
           {
             title: 'React Hooks',
-            path: '/api/react-schema/hooks'
-          }
-        ]
+            path: '/api/react-schema/hooks',
+          },
+        ],
       },
       {
         title: 'Vue API',
@@ -213,17 +219,17 @@ export default {
         children: [
           {
             title: 'Package',
-            path: '/api/vue'
+            path: '/api/vue',
           },
           {
             title: 'Components',
-            path: '/api/vue/components'
+            path: '/api/vue/components',
           },
           {
             title: 'Vue Hooks',
-            path: '/api/vue/hooks'
-          }
-        ]
+            path: '/api/vue/hooks',
+          },
+        ],
       },
       {
         title: 'Vue Schema API',
@@ -231,18 +237,18 @@ export default {
         children: [
           {
             title: 'Package',
-            path: '/api/vue-schema'
+            path: '/api/vue-schema',
           },
           {
             title: 'Components',
-            path: '/api/vue-schema/components'
+            path: '/api/vue-schema/components',
           },
           {
             title: 'Vue Hooks',
-            path: '/api/vue-schema/hooks'
-          }
-        ]
-      }
-    ]
-  }
+            path: '/api/vue-schema/hooks',
+          },
+        ],
+      },
+    ],
+  },
 }
