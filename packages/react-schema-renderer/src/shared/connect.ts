4d902ee48ace43483f3d1e00d3f5bb993b74ca8a
janryWang <wangzhili56@126.com>
2021-01-29 10:24:26 +0800

    fix(schema-renderer): connect receive illegal components

--- packages/react-schema-renderer/src/shared/connect.ts
+++ packages/react-schema-renderer/src/shared/connect.ts
@@ -1,7 +1,7 @@
 import React from 'react'
 import { isArr, each, isFn, isValid, defaults } from '@formily/shared'
 import { useLayout } from '@formily/react'
-import hoistNonReactStatics from 'hoist-non-react-statics';
+import hoistNonReactStatics from 'hoist-non-react-statics'
 import {
   ISchema,
   IConnectOptions,
@@ -153,7 +153,7 @@ export const connect = <ExtendsComponentKey extends string = ''>(
         componentProps.style = {
           ...(componentProps.style || {}),
           width: '100%',
-          flex: '1 1 0%',
+          flex: '1 1 0%'
         }
       }
 
@@ -172,8 +172,9 @@ export const connect = <ExtendsComponentKey extends string = ''>(
     Object.assign(ConnectedComponent, {
       __ALREADY_CONNECTED__: true
     })
-
-    hoistNonReactStatics(ConnectedComponent, Component)
+    if (!Component) {
+      hoistNonReactStatics(ConnectedComponent, Component)
+    }
 
     return ConnectedComponent
   }
