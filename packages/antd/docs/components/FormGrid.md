9afc377724826142b174a20088efebef497d57bc
ZirkleTsing <835702580@qq.com>
2021-02-20 15:29:58 +0800

    feat(FormGrid): update

--- packages/antd/docs/components/FormGrid.md
+++ packages/antd/docs/components/FormGrid.md
@@ -7,8 +7,8 @@
 ```tsx
 import React from 'react'
 import { FormItem, Input, FormGrid } from '@formily/antd'
-
-import { FormProvider, createForm, createSchemaField } from '@formily/react'
+import { FormProvider, createSchemaField } from '@formily/react'
+import { createForm } from '@formily/core'
 
 const SchemaField = createSchemaField({
   components: {
@@ -27,8 +27,8 @@ export default () => {
         <SchemaField.Void
           x-component="FormGrid"
           x-component-props={{
-            minColumns: [4, 6, 10],
-            colWrap: true,
+            maxColumns: 3,
+            minColumns: 2,
           }}
         >
           <SchemaField.String
@@ -67,6 +67,12 @@ export default () => {
             x-decorator="FormItem"
             x-component="Input"
           />
+          <SchemaField.String
+            name="ggg"
+            title="ggg"
+            x-decorator="FormItem"
+            x-component="Input"
+          />
         </SchemaField.Void>
       </SchemaField>
     </FormProvider>
@@ -79,8 +85,8 @@ export default () => {
 ```tsx
 import React from 'react'
 import { FormItem, Input, FormGrid } from '@formily/antd'
-
-import { FormProvider, createForm, createSchemaField } from '@formily/react'
+import { FormProvider, createSchemaField } from '@formily/react'
+import { createForm } from '@formily/core'
 
 const SchemaField = createSchemaField({
   components: {
@@ -99,8 +105,7 @@ const schema = {
       type: 'void',
       'x-component': 'FormGrid',
       'x-component-props': {
-        minColumns: [4, 6, 10],
-        colWrap: true,
+        minColumns: [5, 6, 10],
       },
       properties: {
         aaa: {
@@ -139,6 +144,12 @@ const schema = {
           'x-decorator': 'FormItem',
           'x-component': 'Input',
         },
+        ggg: {
+          type: 'string',
+          title: 'GGG',
+          'x-decorator': 'FormItem',
+          'x-component': 'Input',
+        },
       },
     },
   },
@@ -153,15 +164,193 @@ export default () => {
 }
 ```
 
+## 原生 案例
+
+```tsx
+import React from 'react'
+import { FormGrid } from '@formily/antd'
+
+const { GridColumn } = FormGrid
+const Cell = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: '#AAA',
+        color: '#FFF',
+        height: 30,
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 10px',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+export default () => {
+  return (
+    <React.Fragment>
+      <p>maxColumns 3 + minColumns 2</p>
+      <FormGrid maxColumns={3} minColumns={2}>
+        <GridColumn gridSpan={4}>
+          <Cell>1</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>2</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>3</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>4</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>5</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>6</Cell>
+        </GridColumn>
+      </FormGrid>
+      <p>maxColumns 3</p>
+      <FormGrid maxColumns={3}>
+        <GridColumn gridSpan={2}>
+          <Cell>1</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>2</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>3</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>4</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>5</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>6</Cell>
+        </GridColumn>
+      </FormGrid>
+      <p>minColumns 2</p>
+      <FormGrid minColumns={2}>
+        <GridColumn gridSpan={2}>
+          <Cell>1</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>2</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>3</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>4</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>5</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>6</Cell>
+        </GridColumn>
+      </FormGrid>
+      <p>Null</p>
+      <FormGrid>
+        <GridColumn gridSpan={2}>
+          <Cell>1</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>2</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>3</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>4</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>5</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>6</Cell>
+        </GridColumn>
+      </FormGrid>
+      <p>minWidth 150 +maxColumns 3</p>
+      <FormGrid minWidth={150} maxColumns={3}>
+        <GridColumn gridSpan={2}>
+          <Cell>1</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>2</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>3</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>4</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>5</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>6</Cell>
+        </GridColumn>
+      </FormGrid>
+      <p>maxWidth 120+minColumns 2</p>
+      <FormGrid maxWidth={120} minColumns={2}>
+        <GridColumn gridSpan={2}>
+          <Cell>1</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>2</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>3</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>4</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>5</Cell>
+        </GridColumn>
+        <GridColumn>
+          <Cell>6</Cell>
+        </GridColumn>
+      </FormGrid>
+    </React.Fragment>
+  )
+}
+```
+
 ## API
 
+### FormGrid
+
 | 属性名      | 类型     | 描述                                                                                                             | 默认值                                          |
-| ----------- | -------- | ---------------------------------------------------------------------------------------------------------------- | ----------------------------------------------- | ----- |
-| minWidth    | number   | number[]                                                                                                         | 元素最小宽度, minWidth 生效优先级高于 minColumn |       |
-| maxWidth    | number   | number[]                                                                                                         | 元素最大宽度, maxWidth 优先级高于 maxColumn     |       |
-| minColumns  | number   | number[]                                                                                                         | 最小列数                                        | 0     |
-| maxColumns  | number   | number[]                                                                                                         | 最大列数                                        |       |
-| colWrap     | boolean  | boolean[]                                                                                                        | 是否换行                                        | false |
+| ----------- | -------- | ---------------------------------------------------------------------------------------------------------------- | ----------------------------------------------- | --- |
+| minWidth    | number   | number[]                                                                                                         | 元素最小宽度, minWidth 生效优先级高于 minColumn |     |
+| maxWidth    | number   | number[]                                                                                                         | 元素最大宽度, maxWidth 优先级高于 maxColumn     |     |
+| minColumns  | number   | number[]                                                                                                         | 最小列数                                        | 0   |
+| maxColumns  | number   | number[]                                                                                                         | 最大列数                                        |     |
 | breakpoints | number[] | 容器尺寸断点, minWidth/maxWidth/minColumns/maxColumns 可根据断点分别设置，若无设置，自动向后兼容上一断点的默认值 | [720,1280,1920]                                 |
 | columnGap   | number   | 列间距                                                                                                           | 10                                              |
 | rowGap      | number   | 行间距                                                                                                           | 5                                               |
+
+### FormGrid.GridColumn
+
+| 属性名   | 类型   | 描述         | 默认值 |
+| -------- | ------ | ------------ | ------ |
+| gridSpan | number | 元素所跨列数 | 1      |
+
+### FormGrid.useGridSpan
+
+#### 描述
+
+根据容器宽度计算出正确的 span，防止元素溢出
+
+#### 签名
+
+```ts
+interface uesGridSpan {
+  (gridSpan: number): number
+}
+```
--- packages/next/src/form-grid/index.tsx
+++ packages/next/src/form-grid/index.tsx
@@ -1,8 +1,9 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React, { useLayoutEffect, useRef, useState, useContext } from 'react'
 import { usePrefixCls } from '../__builtins__'
 import cls from 'classnames'
 import { isValid, isNum, isBool } from '@formily/shared'
 import ResizeObserver from 'resize-observer-polyfill'
+import { FormGridContext } from './context';
 
 interface ILayout {
   ref: React.MutableRefObject<HTMLDivElement>
@@ -12,7 +13,10 @@ interface ILayout {
     columns?: number
     colWrap?: boolean
     maxWidth?: number
+    minColumns?: number
+    maxColumns?: number
   }
+  styles: React.CSSProperties
 }
 
 interface ILayoutProps {
@@ -23,6 +27,7 @@ interface ILayoutProps {
   intervals: Array<number[]>
   colWrap: boolean[]
   columnGap: number
+  rowGap: number
 }
 
 interface IFormGridProps {
@@ -31,7 +36,7 @@ interface IFormGridProps {
   minColumns?: number | number[]
   maxColumns?: number | number[]
   colWrap?: boolean | boolean[]
-  breakpoints: number[]
+  breakpoints?: number[]
   columnGap: number
   rowGap: number
 }
@@ -46,8 +51,22 @@ interface IStyleProps extends IFormGridProps {
     columns?: number
     colWrap?: boolean
     maxWidth?: number
+    clientWidth?: number
+    maxColumns?: number
+    minColumns: number
   }
+  ref: React.MutableRefObject<HTMLDivElement>
 }
+
+interface IGridColumnProps {
+  gridSpan: number
+}
+
+type ComposedFormGrid = React.FC<IFormGridProps> & {
+  GridColumn: React.FC<IGridColumnProps>
+  useGridSpan: (gridSpan: number) => number
+}
+
 
 const S = 720
 const MD = 1280
@@ -61,11 +80,14 @@ const useLayout = (props: ILayoutProps): ILayout => {
     minWidth,
     maxWidth,
     colWrap,
+    columnGap,
+    rowGap,
   } = props
   const ref = useRef<HTMLDivElement>(null)
   const formGridPrefixCls = usePrefixCls('formily-grid')
   const observer = useRef(null)
   const [layoutParams, setLayout] = useState({})
+  const [styles, setStyles] = useState({})
 
   const calculateSmartColumns = (target: HTMLElement) => {
     const { clientWidth } = target
@@ -75,42 +97,42 @@ const useLayout = (props: ILayoutProps): ILayout => {
         return true
       }
     })
-
     if (!isValid(maxWidth) && !isValid(maxColumns)) {
-      const minWidthUnderMinColumns = ((clientWidth - (minColumns[index] -1)) * props.columnGap) / minColumns[index]
       return {
         minWidth: isValid(minWidth)
-          ? minWidthUnderMinColumns > minWidth[index]
-            ? minWidth[index]
-            : minWidthUnderMinColumns
-          : isValid(minColumns[index])
-          ? Math.floor(
-              (clientWidth - (minColumns[index] - 1) * props.columnGap) /
-                minColumns[index]
-            )
-          : //
-            0,
+          ? minWidth[index]
+          : isValid(maxColumns[index])
+          ? maxColumns ? Math.floor((clientWidth - (maxColumns[index] - 1) * props.columnGap) / maxColumns[index]) : minWidth
+          : 0,
+        maxWidth: isValid(maxWidth) 
+          ? maxWidth[index] 
+          : Infinity === Math.floor((clientWidth - (minColumns[index] - 1) * props.columnGap) / minColumns[index])
+          ? clientWidth
+          : Math.floor((clientWidth - (minColumns[index] - 1) * props.columnGap) / minColumns[index]),
         columns: target.childNodes.length,
         colWrap: colWrap[index],
+        minColumns: minColumns ? minColumns[index] : 1,
+        maxColumns: maxColumns ? maxColumns[index] : undefined,
+        clientWidth,
       }
     } else {
       // maxWidth 优先级 高于maxColumns
-      const minWidthUnderMinColumns = ((clientWidth - (minColumns[index] -1)) * props.columnGap) / minColumns[index]
+      // const minWidthUnderMinColumns = (clientWidth - (maxColumns[index] - 1) * props.columnGap) / maxColumns[index]
       return {
-        maxWidth: isValid(maxWidth) ? maxWidth[index] : Math.floor((clientWidth - (maxColumns[index] - 1) * props.columnGap) / maxColumns[index]),
+        maxWidth: isValid(maxWidth) 
+          ? maxWidth[index] 
+          : Infinity === Math.floor((clientWidth - (minColumns[index] - 1) * props.columnGap) / minColumns[index])
+            ? clientWidth
+            : Math.floor((clientWidth - (minColumns[index] - 1) * props.columnGap) / minColumns[index]),
         minWidth: isValid(minWidth)
-          ? minWidthUnderMinColumns > minWidth[index]
-            ? minWidth[index]
-            : minWidthUnderMinColumns
-          : isValid(minColumns[index])
-          ? Math.floor(
-              (clientWidth - (minColumns[index] - 1) * props.columnGap) /
-                minColumns[index]
-            )
-          : //
-            0,
+          ? minWidth[index]
+          : isValid(maxColumns[index])
+          ? Math.floor((clientWidth - (maxColumns[index] - 1) * props.columnGap) / maxColumns[index]): 0,
+        minColumns: minColumns ? minColumns[index] : 1,
+        maxColumns: maxColumns ? maxColumns[index] : undefined,
         columns: target.childNodes.length,
-        colWrap: colWrap[index]
+        colWrap: colWrap[index],
+        clientWidth,
       }
     }
   }
@@ -122,8 +144,9 @@ const useLayout = (props: ILayoutProps): ILayout => {
           entries.forEach((entry) => {
             const target = entry.target as HTMLElement
             const params = calculateSmartColumns(target)
-            // setColumns(columns)
             setLayout(params)
+            const style = getStyle({ columnGap, rowGap, layoutParams: params, ref })
+            setStyles(style)
           })
         // })
       }) as any
@@ -137,13 +160,14 @@ const useLayout = (props: ILayoutProps): ILayout => {
     ref,
     formGridPrefixCls,
     layoutParams,
+    styles
   }
 }
 
-const useStyle = (props: IStyleProps): IStyle => {
-  const { columnGap, rowGap, layoutParams } = props
-  const max = layoutParams.maxWidth ? `${layoutParams.maxWidth}px` : '1fr';
-
+const getStyle = (props: IStyleProps): IStyle => {
+  const { columnGap, rowGap, layoutParams, ref } = props
+  // const max = layoutParams.maxWidth ? `${layoutParams.maxWidth}px` : '1fr';
+  const { clientWidth, minWidth, maxColumns, minColumns } = layoutParams
   const getMinMax = (minWidth: number, maxWidth: number) => {
     let minmax 
     if (minWidth === Infinity) {
@@ -157,20 +181,37 @@ const useStyle = (props: IStyleProps): IStyle => {
     }
     return minmax
   }
+  
+  const spans = Array.from(ref.current?.childNodes || []).reduce((buf, cur: HTMLElement) => {
+    const span = isValid(maxColumns) ? Math.min((Number(cur.getAttribute('data-span')) || 1) as number, maxColumns) : (Number(cur.getAttribute('data-span')) || 1)
+    return buf + Number(span)
+  }, 0)
+
+  const calc = Math.floor((clientWidth + columnGap) / (minWidth + columnGap))
+  let finalColumns
+  finalColumns = Math.min(spans, calc)
+  if (isValid(maxColumns)) {
+    finalColumns = Math.min(spans, calc, maxColumns)
+  } else {
+
+    finalColumns = Math.min(spans, calc)
+  }
+
+  if (isValid(minColumns)) {
+    if(finalColumns < minColumns) {
+      finalColumns = minColumns
+    }
+  }
+
   const style = {
-        // 如果 colWarp true minWidth生效优先级高于minColumn 并且必须给minWidth | minColumn
-        // 如果 colWarap false minWidth生效优先级高于minColumn
-        // maxWidth 优先级高于 maxColumn
-        gridTemplateColumns: layoutParams.colWrap
-          ? `repeat(auto-fill, minmax(${layoutParams.minWidth}px,${max}))`
-          // : `repeat(${layoutParams.columns},${layoutParams.minWidth === Infinity ? '1fr' : `minmax(${layoutParams.minWidth}px,${max})`})`,
-          : `repeat(${layoutParams.columns}, ${getMinMax(layoutParams.minWidth, layoutParams.maxWidth)})`,
+        gridTemplateColumns: 
+          `repeat(${finalColumns}, ${getMinMax(layoutParams.minWidth, layoutParams.maxWidth)})`,
         gridGap: `${rowGap}px ${columnGap}px`,
       }
   return style
 }
 
-export const FormGrid: React.FC<IFormGridProps> = (props) => {
+export const FormGrid: ComposedFormGrid = (props) => {
   const normalizeProps = (props: IFormGridProps): ILayoutProps => {
     const { breakpoints } = props
 
@@ -209,36 +250,81 @@ export const FormGrid: React.FC<IFormGridProps> = (props) => {
       maxColumns: normalize(props.maxColumns),
     }
   }
-  const { children, columnGap, rowGap, breakpoints } = props
+  const { children } = props
   const normalizedProps = normalizeProps(props)
-  const { ref, formGridPrefixCls, layoutParams } = useLayout(
+  const { ref, formGridPrefixCls, layoutParams, styles } = useLayout(
     normalizedProps
   )
-
-  const styles = useStyle({
-    columnGap,
-    rowGap,
-    breakpoints,
-    layoutParams,
-  })
-
   return (
-    <div
-      className={cls(`${formGridPrefixCls}-layout`)}
-      style={styles}
-      ref={ref}
+    <FormGridContext.Provider
+      value={{columnGap: props.columnGap, ...layoutParams}}
     >
-      {children}
-    </div>
+      <div
+        className={cls(`${formGridPrefixCls}-layout`)}
+        style={styles}
+        ref={ref}
+      >
+        {children}
+      </div>
+    </FormGridContext.Provider>
   )
 }
+
+const GridColumn: React.FC<IGridColumnProps> = ({ gridSpan, children }) => {
+ 
+  const span = FormGrid.useGridSpan(gridSpan)
+  return (
+    <div style={{ gridColumnStart: `span ${span}` }} data-span={span || 1}>{children}</div>
+  )
+}
+
+
+GridColumn.defaultProps = {
+  gridSpan: 1
+}
+
+FormGrid.useGridSpan = (gridSpan: number) => {
+  const params = useContext(FormGridContext);
+  if (!isValid(params)) {
+    return gridSpan
+  }
+  const { colWrap, columns, clientWidth, minWidth, columnGap, maxColumns } = params
+  const calc = Math.floor((clientWidth + columnGap) / (minWidth + columnGap)) // 算出实际一行最多能塞进的格子数
+  if (colWrap === true) {
+    if (Math.min(calc, columns) >= gridSpan) {
+      if (isValid(maxColumns)) {
+        return Math.min(gridSpan, maxColumns)
+      }
+     return gridSpan
+    } else {
+      if (isValid(maxColumns)) {
+        return Math.min(calc, columns, maxColumns)
+      }
+      return Math.min(calc, columns)
+    }
+  } else {
+    if (Math.min(calc, columns) >= gridSpan) {
+      if (isValid(maxColumns)) {
+        return Math.min(gridSpan, maxColumns)
+      }
+      return gridSpan
+    } else {
+      if (isValid(maxColumns)) {
+        return Math.min(calc,columns, maxColumns)
+      }
+      return Math.min(calc,columns)
+    } 
+  }
+}
+FormGrid.GridColumn = GridColumn
 
 FormGrid.defaultProps = {
   columnGap: 10,
   rowGap: 5,
   minColumns: 0,
+  minWidth: 100,
   breakpoints: [S, MD, LG],
-  colWrap: false,
+  colWrap: true,
 }
 
 export default FormGrid
