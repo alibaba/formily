a20e2a3c577053ad0dd1c157d97e76ff793f1837
atzcl <atzcl0310@gmail.com>
2021-03-30 17:09:44 +0800

    chore(next): replace lib with es

--- packages/next/copy.ts
+++ packages/next/copy.ts
@@ -1,12 +1,61 @@
-import { copy } from 'fs-extra'
+import { copy, readFile, writeFile } from 'fs-extra'
 import glob from 'glob'
 
-glob(`./src/**/*.{less,scss}`, (err, files) => {
-  if (err) {
-    throw err
-  }
-  files.forEach((filename) => {
-    copy(filename, filename.replace(/src\//, 'esm/'))
-    copy(filename, filename.replace(/src\//, 'lib/'))
+const copyStyle = () =>
+  new Promise((resolve, reject) => {
+    glob(`./src/**/*.{less,scss}`, (err, files) => {
+      if (err) {
+        return reject(err)
+      }
+
+      return Promise.all(
+        files.reduce(
+          (reproduces, filename) => [
+            ...reproduces,
+            copy(filename, filename.replace(/src\//, 'esm/')),
+            copy(filename, filename.replace(/src\//, 'lib/')),
+          ],
+          [] as Promise<void>[]
+        )
+      )
+        .then(resolve)
+        .catch(reject)
+    })
   })
-})
+
+const esStr = '@alifd/next/es/'
+const libStr = '@alifd/next/lib/'
+
+const replaceLibWithEsRegExp = (flags?: 'g' | 'i' | 'm' | 'u' | 'y') =>
+  new RegExp(libStr, flags)
+
+const replaceLibWithEs = () =>
+  new Promise((resolve, reject) => {
+    glob(`./esm/**/style.js`, (err, files) => {
+      if (err) {
+        return reject(err)
+      }
+
+      const replaceOperation = async (filename: string) => {
+        const fileContent: string = (await readFile(filename)).toString()
+
+        if (!replaceLibWithEsRegExp().test(fileContent)) {
+          return Promise.resolve()
+        }
+
+        return writeFile(
+          filename,
+          fileContent.replace(replaceLibWithEsRegExp('g'), esStr)
+        )
+      }
+
+      return Promise.all(files.map((filename) => replaceOperation(filename)))
+        .then(resolve)
+        .catch(reject)
+    })
+  })
+
+;(async () => {
+  await copyStyle()
+  await replaceLibWithEs()
+})()
