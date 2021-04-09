c84cb866f11b6b4831b97021dbe4cc99da5940dd
atzcl <atzcl0310@gmail.com>
2021-04-09 10:55:49 +0800

    refactor(next): use build style

---
+++ packages/next/build-style.ts
@@ -0,0 +1,12 @@
+import { build } from '../../scripts/build-style'
+
+build({
+  esStr: '@alifd/next/es/',
+  libStr: '@alifd/next/lib/',
+  styleEntry: 'main.scss',
+  // antd/es/button/style/index ===> antd/es/button/style/css
+  importCssCompilerToCssTransform: (v) =>
+    v.replace(/'@alifd\/next\/(es|lib)\/(.*)'/g, (subStr) =>
+      subStr.replace(/\/style'$/, `/index.css'`)
+    ),
+})
