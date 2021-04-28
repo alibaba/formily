b1eab98a779051ac403b0825b2217ce23467a744
janrywang <wangzhili56@126.com>
2021-04-28 14:50:30 +0800

    feat(react): add context cleaner to FormProvider

--- packages/react/src/shared/context.ts
+++ packages/react/src/shared/context.ts
@@ -1,6 +1,14 @@
-import { createContext } from 'react'
+import React, { createContext } from 'react'
 import { Schema } from '@formily/json-schema'
 import { ISchemaFieldFactoryOptions } from '../types'
+
+const createContextCleaner = <T>(...contexts: React.Context<T>[]) => {
+  return ({ children }) => {
+    return contexts.reduce((buf, ctx) => {
+      return React.createElement(ctx.Provider, { value: undefined }, buf)
+    }, children)
+  }
+}
 
 export const FormContext = createContext<Formily.Core.Models.Form>(null)
 export const FieldContext = createContext<Formily.Core.Types.GeneralField>(null)
@@ -9,4 +17,12 @@ export const SchemaContext = createContext<Schema>(null)
 export const SchemaExpressionScopeContext = createContext<any>(null)
 export const SchemaOptionsContext = createContext<ISchemaFieldFactoryOptions>(
   null
+)
+
+export const ContextCleaner = createContextCleaner(
+  FieldContext,
+  SchemaMarkupContext,
+  SchemaContext,
+  SchemaExpressionScopeContext,
+  SchemaOptionsContext
 )
