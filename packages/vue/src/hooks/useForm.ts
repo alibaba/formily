6d176852aed95caf3f00bf644d43d4408c405558
MisicDemone <424532913@qq.com>
2021-06-05 21:33:54 +0800

    fix(vue): useForm/useField  will never get null and instead to be a ref

--- packages/vue/src/hooks/useForm.ts
+++ packages/vue/src/hooks/useForm.ts
@@ -1,8 +1,8 @@
-import { inject, Ref } from 'vue-demi'
+import { inject, Ref, ref } from 'vue-demi'
 import { FormSymbol } from '../shared/context'
 
 export const useForm = (): Ref<Formily.Core.Models.Form> => {
-  const form = inject(FormSymbol, null)
+  const form = inject(FormSymbol, ref())
   if (!form) {
     throw new Error('Can not found form instance from context.')
   }
