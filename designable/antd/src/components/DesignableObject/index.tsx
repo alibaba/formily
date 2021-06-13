926d2f5923097b718d9443fbd05a1b7b770711d2
janrywang <zhili.wzl@alibaba-inc.com>
2021-06-13 10:52:23 +0800

    chore(designable-antd): upgrade designable

--- designable/antd/src/components/DesignableObject/index.tsx
+++ designable/antd/src/components/DesignableObject/index.tsx
@@ -6,9 +6,5 @@ export const DesignableObject: React.FC = (props) => {
   const node = useTreeNode()
   const nodeId = useNodeIdProps()
   if (node.children.length === 0) return <Droppable {...nodeId} />
-  return (
-    <div {...nodeId} style={{ paddingTop: 10, paddingBottom: 10 }}>
-      {props.children}
-    </div>
-  )
+  return <div {...nodeId}>{props.children}</div>
 }
