6b33ec9cc86e1601b237341f4c80b8f3823fcc11
后浪 <9269391@qq.com>
2021-06-24 11:11:43 +0800

    feat(antd): improve Submit API (#1640)

--- packages/antd/src/submit/index.tsx
+++ packages/antd/src/submit/index.tsx
@@ -4,12 +4,14 @@ import { ButtonProps } from 'antd/lib/button'
 import { useForm, observer } from '@formily/react'
 
 export interface ISubmitProps extends ButtonProps {
+  onClick?: (e: React.MouseEvent<Element, MouseEvent>) => boolean | void
   onSubmit?: (values: any) => Promise<any> | any
+  onSubmitSuccess?: (payload: any) => void
   onSubmitFailed?: (feedbacks: Formily.Core.Types.IFormFeedback[]) => void
 }
 
 export const Submit: React.FC<ISubmitProps> = observer(
-  ({ onSubmit, onSubmitFailed, ...props }: ISubmitProps) => {
+  ({ onSubmit, onSubmitFailed, onSubmitSuccess, ...props }: ISubmitProps) => {
     const form = useForm()
     return (
       <Button
@@ -19,10 +21,10 @@ export const Submit: React.FC<ISubmitProps> = observer(
         loading={props.loading !== undefined ? props.loading : form.submitting}
         onClick={(e) => {
           if (props.onClick) {
-            props.onClick(e)
+            if (props.onClick(e) === false) return
           }
           if (onSubmit) {
-            form.submit(onSubmit).catch(onSubmitFailed)
+            form.submit(onSubmit).then(onSubmitSuccess).catch(onSubmitFailed)
           }
         }}
       >
