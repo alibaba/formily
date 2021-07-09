2be3a10d50c30c6e6391c237249470c38b82deeb
janrywang <zhili.wzl@alibaba-inc.com>
2021-07-09 23:08:01 +0800

    feat(antd): transfer compat label/value

--- packages/antd/src/transfer/index.tsx
+++ packages/antd/src/transfer/index.tsx
@@ -1,12 +1,28 @@
 import { connect, mapProps } from '@formily/react'
 import { Transfer as AntdTransfer } from 'antd'
+import { isVoidField } from '@formily/core'
 
 export const Transfer = connect(
   AntdTransfer,
-  mapProps({
-    value: 'targetKeys',
-    dataSource: true,
-  })
+  mapProps(
+    {
+      value: 'targetKeys',
+    },
+    (props, field) => {
+      if (isVoidField(field)) return props
+      return {
+        ...props,
+        dataSource:
+          field.dataSource?.map((item) => {
+            return {
+              ...item,
+              title: item.title || item.label,
+              key: item.key || item.value,
+            }
+          }) || [],
+      }
+    }
+  )
 )
 
 Transfer.defaultProps = {
