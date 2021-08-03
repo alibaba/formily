bad4d4ec63bd045f5ce37c73acea60761357037c
Grapedge <shiwang.lj@alibaba-inc.com>
2021-08-03 10:18:40 +0800

    fix(next): fix Select cannot be selected when children is empty

--- packages/next/src/select/index.tsx
+++ packages/next/src/select/index.tsx
@@ -1,13 +1,33 @@
 import { connect, mapReadPretty, mapProps } from '@formily/react'
+import { isVoidField } from '@formily/core'
 import { Select as NextSelect } from '@alifd/next'
 import { PreviewText } from '../preview-text'
 import { mapSize, mapStatus } from '../__builtins__'
 
+const patchDataSource = (dataSource: any = []) => {
+  const removeEmptyChildren = (data: any) => {
+    const result = { ...data }
+    if (!result.children || result.children.length === 0) {
+      delete result.children
+    } else {
+      result.children = result.children.map(removeEmptyChildren)
+    }
+    return result
+  }
+  return dataSource.map(removeEmptyChildren)
+}
+
 export const Select = connect(
   NextSelect,
   mapProps(
-    {
-      dataSource: true,
+    (props, field) => {
+      if (isVoidField(field)) {
+        return props
+      }
+      return {
+        ...props,
+        dataSource: patchDataSource(props.dataSource ?? field?.dataSource),
+      }
     },
     mapSize,
     mapStatus
