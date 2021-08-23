9ece25fb831e01e091e69d1744524c6c6216b454
janrywang <zhili.wzl@alibaba-inc.com>
2021-08-23 11:51:19 +0800

    chore(designable): use data-content-editable attrinbute for DesignableText

--- designable/antd/src/components/DesignableText/index.tsx
+++ designable/antd/src/components/DesignableText/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react'
-import ContentEditable from 'react-contenteditable'
-import { useTreeNode } from '@designable/react'
+import React from 'react'
+import { useDesigner } from '@designable/react'
 import cls from 'classnames'
 import './styles.less'
 
@@ -11,54 +10,16 @@ export interface IDesignableTextProps {
   className?: string
 }
 
-function placeCaretAtEnd(el: HTMLInputElement) {
-  el.focus()
-  let range = document.createRange()
-  range.selectNodeContents(el)
-  range.collapse(false)
-  let sel = window.getSelection()
-  sel.removeAllRanges()
-  sel.addRange(range)
-}
-
-export const DesignableText: React.FC<IDesignableTextProps> = ({
-  content,
-  mode,
-  ...props
-}) => {
-  const node = useTreeNode()
-  const ref = useRef<HTMLInputElement>()
-  const [html, setHTML] = useState(content ?? 'Please Input')
-  const [active, setActive] = useState(false)
-  const tagName = mode === 'normal' || !mode ? 'div' : mode
-  useEffect(() => {
-    if (content && content !== html) {
-      setHTML(content)
-    }
-  }, [content, html])
-  return (
-    <ContentEditable
-      {...props}
-      innerRef={ref}
-      disabled={!active}
-      className={cls(props.className, 'dn-text', { active })}
-      tagName={tagName}
-      html={html}
-      onKeyDown={(e) => e.key === 'Enter' && e.preventDefault()}
-      onChange={(e) => {
-        setHTML(e.target.value)
-        node.props['x-component-props'] = node.props['x-component-props'] || {}
-        node.props['x-component-props'].content = e.target.value
-      }}
-      onDoubleClick={() => {
-        setActive(true)
-        setTimeout(() => {
-          placeCaretAtEnd(ref.current)
-        }, 16)
-      }}
-      onBlur={() => {
-        setActive(false)
-      }}
-    />
+export const DesignableText: React.FC<IDesignableTextProps> = (props) => {
+  const designer = useDesigner()
+  const tagName = props.mode === 'normal' || !props.mode ? 'div' : props.mode
+  return React.createElement(
+    tagName,
+    {
+      ...props,
+      className: cls(props.className, 'dn-text'),
+      [designer.props.contentEditableAttrName]: 'x-component-props.content',
+    },
+    props.content
   )
 }
--- designable/next/src/components/DesignableText/index.tsx
+++ designable/next/src/components/DesignableText/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react'
-import ContentEditable from 'react-contenteditable'
-import { useTreeNode } from '@designable/react'
+import React from 'react'
+import { useDesigner } from '@designable/react'
 import cls from 'classnames'
 import './styles.less'
 
@@ -11,50 +10,16 @@ export interface IDesignableTextProps {
   className?: string
 }
 
-function placeCaretAtEnd(el: HTMLInputElement) {
-  el.focus()
-  let range = document.createRange()
-  range.selectNodeContents(el)
-  range.collapse(false)
-  let sel = window.getSelection()
-  sel.removeAllRanges()
-  sel.addRange(range)
-}
-
 export const DesignableText: React.FC<IDesignableTextProps> = (props) => {
-  const node = useTreeNode()
-  const ref = useRef<HTMLInputElement>()
-  const [html, setHTML] = useState(props.content ?? 'Please Input')
-  const [active, setActive] = useState(false)
+  const designer = useDesigner()
   const tagName = props.mode === 'normal' || !props.mode ? 'div' : props.mode
-  useEffect(() => {
-    if (props.content && props.content !== html) {
-      setHTML(props.content)
-    }
-  }, [props.content, html])
-  return (
-    <ContentEditable
-      {...props}
-      innerRef={ref}
-      disabled={!active}
-      className={cls(props.className, 'dn-text', { active })}
-      tagName={tagName}
-      html={html}
-      onKeyDown={(e) => e.key === 'Enter' && e.preventDefault()}
-      onChange={(e) => {
-        setHTML(e.target.value)
-        node.props['x-component-props'] = node.props['x-component-props'] || {}
-        node.props['x-component-props'].content = e.target.value
-      }}
-      onDoubleClick={() => {
-        setActive(true)
-        setTimeout(() => {
-          placeCaretAtEnd(ref.current)
-        }, 16)
-      }}
-      onBlur={() => {
-        setActive(false)
-      }}
-    />
+  return React.createElement(
+    tagName,
+    {
+      ...props,
+      className: cls(props.className, 'dn-text'),
+      [designer.props.contentEditableAttrName]: 'x-component-props.content',
+    },
+    props.content
   )
 }
