3b8740371111b1a8c92b91e56aa3999f35dda002
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-19 07:35:49 +0800

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require('./scripts/jest.base')
-// jest.config.js
-
-// Note: If you are using babel version 7 you have to install babel-jest with
-// yarn add --dev babel-jest 'babel-core@^7.0.0-bridge' @babel/core
-
-module.exports = {
-  ...baseConfig,
-}
