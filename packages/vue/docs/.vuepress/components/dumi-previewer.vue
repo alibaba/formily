07739bb92cf03cde9c834cadb190b75fc49d04a7
Muyao <wanngxiao@gmail.com>
2021-09-21 08:52:50 +0800

    docs(vue/element): add codesandbox support (#2206)

--- packages/vue/docs/.vuepress/components/dumi-previewer.vue
+++ packages/vue/docs/.vuepress/components/dumi-previewer.vue
@@ -12,7 +12,20 @@
       </div>
 
       <div class="dumi-previewer-actions">
-        <div></div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            class="dumi-previewer-actions__icon"
+            viewBox="0 0 256 296"
+            @click="openCodeSandBox"
+          >
+            <path
+              d="M115.498 261.088v-106.61L23.814 101.73v60.773l41.996 24.347v45.7l49.688 28.54zm23.814.627l50.605-29.151V185.78l42.269-24.495v-60.011l-92.874 53.621v106.82zm80.66-180.887l-48.817-28.289l-42.863 24.872l-43.188-24.897l-49.252 28.667l91.914 52.882l92.206-53.235zM0 222.212V74.495L127.987 0L256 74.182v147.797l-128.016 73.744L0 222.212z"
+              fill="#000"
+            ></path>
+          </svg>
+        </div>
 
         <div>
           <svg
@@ -71,6 +84,7 @@
 <script>
 import copy from 'copy-to-clipboard'
 import highlight from './highlight'
+import { createCodeSandBox } from './createCodeSandBox'
 
 export default {
   name: 'dumi-previewer',
@@ -143,6 +157,10 @@ export default {
       this.timerId = setTimeout(() => {
         this.copied = false
       }, 2000)
+    },
+
+    openCodeSandBox() {
+      createCodeSandBox(this.demoStr)
     },
   },
 }
