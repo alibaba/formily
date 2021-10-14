8a7244a2c721185798137a7ea97fd8e1d1dcc255
janrywang <zhili.wzl@alibaba-inc.com>
2021-10-14 13:58:29 +0800

    refactor(react): silent useForm for child form sence

--- packages/react/src/hooks/useForm.ts
+++ packages/react/src/hooks/useForm.ts
@@ -3,9 +3,5 @@ import { Form } from '@formily/core'
 import { FormContext } from '../shared'
 
 export const useForm = <T extends object = any>(): Form<T> => {
-  const form = useContext(FormContext)
-  if (!form) {
-    throw new Error('Can not found form instance from context.')
-  }
-  return form
+  return useContext(FormContext)
 }
