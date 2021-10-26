9ef3d3bda5aee899066ef3990cee4b3e1bc123d7
hjkcai <huajingkun1214@gmail.com>
2021-10-26 11:06:54 +0800

    fix(react): fix incorrect dts in useFieldSchema

--- packages/react/src/hooks/useFieldSchema.ts
+++ packages/react/src/hooks/useFieldSchema.ts
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { SchemaContext } from '../shared'
+import { Schema } from '@formily/json-schema'
 
-export const useFieldSchema = () => {
+export const useFieldSchema = (): Schema => {
   return useContext(SchemaContext)
 }
