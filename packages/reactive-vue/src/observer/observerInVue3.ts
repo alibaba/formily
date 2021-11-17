cd50ad7d9e9236d9898167d3ffa015f7f5e139b9
Muyao <wanngxiao@gmail.com>
2021-11-17 22:28:31 +0800

    fix(element): fix FormLayout provide props not update  (#2448)
    * fix(element): fix FormLayout provide props not update
    
    * chore(reactive-vue): ignore vue3 test branch

--- packages/reactive-vue/src/observer/observerInVue3.ts
+++ packages/reactive-vue/src/observer/observerInVue3.ts
@@ -1,6 +1,7 @@
 import { IObserverOptions } from '../types'
 import { useObserver } from '../hooks/useObserver'
 
+/* istanbul ignore next */
 export const observer = function (opts: any, options?: IObserverOptions): any {
   const name = options?.name || opts.name || 'ObservableComponent'
 
