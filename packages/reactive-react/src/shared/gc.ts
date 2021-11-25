9d9d5e3554e1ceb0833a11660433c8da35d2a389
Yiliang <809050830@qq.com>
2021-11-25 15:42:56 +0800

    chore: add ESNext and DOM lib to TS compiler options

--- packages/reactive-react/src/shared/gc.ts
+++ packages/reactive-react/src/shared/gc.ts
@@ -9,7 +9,7 @@ const registry: FinalizationRegistry<any> =
 type Token = { clean: () => void }
 export class GarbageCollector<T extends object = any> {
   private expireTime: number
-  private request: NodeJS.Timeout
+  private request?: ReturnType<typeof setTimeout>;
   private token: Token
   constructor(clean?: () => void, expireTime = 10_000) {
     this.token = {
