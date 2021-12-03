f5e95bdebb69c60ae48d9bc67b27a968deac6937
ifblooms <qmzmxfy@vip.qq.com>
2021-12-03 18:11:46 +0800

    fix(next): fix missing ExclamationCircleOutlinedIcon bug

--- packages/next/src/__builtins__/icons.tsx
+++ packages/next/src/__builtins__/icons.tsx
@@ -88,7 +88,12 @@ export const CheckCircleOutlinedIcon: IconType = React.forwardRef(
   )
 )
 export const ExclamationCircleOutlinedIcon: IconType = React.forwardRef(
-  (props, ref) => <Icon {...props} ref={ref}></Icon>
+  (props, ref) => (
+    <Icon {...props} ref={ref}>
+      <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
+      <path d="M464 688a48 48 0 1096 0 48 48 0 10-96 0zm24-112h48c4.4 0 8-3.6 8-8V296c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8z"></path>
+    </Icon>
+  )
 )
 
 export const EditOutlinedIcon: IconType = React.forwardRef((props, ref) => (
