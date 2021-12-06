483f79f1066b491b4a62eeb92772897f26bf74e4
Dark <dark.lijin@gmail.com>
2021-12-06 16:12:41 +0800

    docs: improve site show brandName (#2574)
    * docs: site show brandName
    
    * docs: update site styles

--- packages/core/.umirc.js
+++ packages/core/.umirc.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 export default {
   mode: 'site',
   logo: 'https://img.alicdn.com/imgextra/i2/O1CN01Kq3OHU1fph6LGqjIz_!!6000000004056-55-tps-1141-150.svg',
-  title: 'Formily',
+  title: 'Core',
   hash: true,
   favicon:
     'https://img.alicdn.com/imgextra/i3/O1CN01XtT3Tv1Wd1b5hNVKy_!!6000000002810-55-tps-360-360.svg',
@@ -47,10 +47,13 @@ export default {
   },
   styles: [
     `.__dumi-default-navbar-logo{
-      height: 60px !important;
-      width: 150px !important;
-      padding-left:0 !important;
-      color: transparent !important;
+      background-size: 140px!important;
+      background-position: center left!important;
+      background-repeat: no-repeat!important;
+      padding-left: 150px!important;/*可根据title的宽度调整*/
+      font-size: 22px!important;
+      color: #000!important;
+      font-weight: lighter!important;
     }
     .__dumi-default-navbar{
       padding: 0 28px !important;
