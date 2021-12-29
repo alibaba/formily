5d2304c430e3475233a14437ee50ffeef982202e
henryzp <Henry_zp@163.com>
2021-12-29 13:35:04 +0800

    fix(next-component): fix tool methods and complete unit tests

--- packages/next/__tests__/moment.spec.ts
+++ packages/next/__tests__/moment.spec.ts
@@ -1,10 +1,38 @@
 import { formatMomentValue } from '../src/__builtins__/moment'
 
 test('formatMomentValue is usable', () => {
+  expect(formatMomentValue('', 'YYYY-MM-DD', '~')).toBe('~')
   expect(formatMomentValue('2021-12-22 15:47:00', 'YYYY-MM-DD')).toBe(
     '2021-12-22'
   )
   expect(formatMomentValue('2021-12-23 15:47:00', undefined)).toBe(
     '2021-12-23 15:47:00'
   )
+  expect(formatMomentValue('2021-12-21 15:47:00', (date: string) => date)).toBe(
+    '2021-12-21 15:47:00'
+  )
+  expect(
+    formatMomentValue(
+      ['2021-12-21 15:47:00', '2021-12-29 15:47:00'],
+      'YYYY-MM-DD'
+    )
+  ).toEqual(['2021-12-21', '2021-12-29'])
+  expect(
+    formatMomentValue(
+      ['2021-12-21 16:47:00', '2021-12-29 18:47:00'],
+      (date: string) => date
+    )
+  ).toEqual(['2021-12-21 16:47:00', '2021-12-29 18:47:00'])
+  expect(
+    formatMomentValue(
+      ['2021-12-21 16:47:00', '2021-12-29 18:47:00'],
+      ['YYYY-MM-DD', (date: string) => date]
+    )
+  ).toEqual(['2021-12-21', '2021-12-29 18:47:00'])
+  expect(
+    formatMomentValue(
+      ['2021-12-21 16:47:00', '2021-12-29 18:47:00'],
+      ['YYYY-MM-DD', undefined]
+    )
+  ).toEqual(['2021-12-21', '2021-12-29 18:47:00'])
 })
