475d10e918e831721c774bb87765469a84297036
小翼 <Henry_zp@163.com>
2021-12-29 14:23:35 +0800

    fix(antd/next): fix tool methods and provide simple unit tests (#2694)

---
+++ packages/antd/__tests__/moment.spec.ts
@@ -0,0 +1,48 @@
+import moment from 'moment'
+import { formatMomentValue, momentable } from '../src/__builtins__/moment'
+
+test('momentable is usable', () => {
+  expect(moment.isMoment(momentable('2021-09-08'))).toBe(true)
+  expect(
+    momentable(['2021-09-08', '2021-12-29']).every((item) =>
+      moment.isMoment(item)
+    )
+  ).toBe(true)
+})
+
+test('formatMomentValue is usable', () => {
+  expect(formatMomentValue('', 'YYYY-MM-DD', '~')).toBe('~')
+  expect(formatMomentValue('2021-12-21 15:47:00', 'YYYY-MM-DD')).toBe(
+    '2021-12-21'
+  )
+  expect(formatMomentValue('2021-12-21 15:47:00', undefined)).toBe(
+    '2021-12-21 15:47:00'
+  )
+  expect(formatMomentValue('2021-12-21 15:47:00', (date: string) => date)).toBe(
+    '2021-12-21 15:47:00'
+  )
+  expect(
+    formatMomentValue(
+      ['2021-12-21 15:47:00', '2021-12-29 15:47:00'],
+      'YYYY-MM-DD'
+    )
+  ).toEqual(['2021-12-21', '2021-12-29'])
+  expect(
+    formatMomentValue(
+      ['2021-12-21 16:47:00', '2021-12-29 18:47:00'],
+      (date: string) => date
+    )
+  ).toEqual(['2021-12-21 16:47:00', '2021-12-29 18:47:00'])
+  expect(
+    formatMomentValue(
+      ['2021-12-21 16:47:00', '2021-12-29 18:47:00'],
+      ['YYYY-MM-DD', (date: string) => date]
+    )
+  ).toEqual(['2021-12-21', '2021-12-29 18:47:00'])
+  expect(
+    formatMomentValue(
+      ['2021-12-21 16:47:00', '2021-12-29 18:47:00'],
+      ['YYYY-MM-DD', undefined]
+    )
+  ).toEqual(['2021-12-21', '2021-12-29 18:47:00'])
+})
