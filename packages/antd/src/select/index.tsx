994180d8b2f243886840b77099f43a20591ea8e0
rash <rashuorz@outlook.com>
2022-01-06 21:41:04 +0800

    chore(antd): improve typings (#2733)

--- packages/antd/src/select/index.tsx
+++ packages/antd/src/select/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { connect, mapReadPretty, mapProps } from '@formily/react'
 import { Select as AntdSelect } from 'antd'
+import { SelectProps } from 'antd/lib/select'
 import { PreviewText } from '../preview-text'
 import { LoadingOutlined } from '@ant-design/icons'
-export const Select = connect(
+export const Select: React.FC<SelectProps<any>> = connect(
   AntdSelect,
   mapProps(
     {
