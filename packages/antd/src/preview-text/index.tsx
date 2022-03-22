9990b66d8603f3bbbcf6933e15ea1ed714c382cc
Lyca <qmzmxfy@vip.qq.com>
2022-03-22 16:49:02 +0800

    fix(antd/next): fix PreviewText.Cascader bug (#2969)

--- packages/antd/src/preview-text/index.tsx
+++ packages/antd/src/preview-text/index.tsx
@@ -213,13 +213,15 @@ const Cascader: React.FC<CascaderProps> = observer((props) => {
     ? props.options
     : []
   const getSelected = () => {
-    return isArr(props.value) ? props.value : [props.value]
+    return props.multiple
+      ? props.value.map((item) => item[item.length - 1])
+      : props.value.slice(props.value.length - 1)
   }
   const getLabels = () => {
     const selected = getSelected()
     const labels = getValueByValue(dataSource, selected)
       ?.filter((item) => isValid(item))
-      ?.map((item) => item?.whole.join('/'))
+      ?.map((item) => item?.whole?.join('/'))
       .join(', ')
     return labels || placeholder
   }
