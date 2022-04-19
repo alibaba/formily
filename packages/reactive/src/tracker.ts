4a8760fa6d8378142a9ba0eca0e549dbec02ca59
Janry <zhili.wzl@alibaba-inc.com>
2022-04-19 17:45:09 +0800

    feat(reactive): remove reactive tracker default batch (#3052)

--- packages/reactive/src/tracker.ts
+++ packages/reactive/src/tracker.ts
@@ -1,12 +1,7 @@
 import { ReactionStack } from './environment'
 import { isFn } from './checkers'
 import { Reaction } from './types'
-import {
-  batchEnd,
-  batchStart,
-  disposeBindingReactions,
-  releaseBindingReactions,
-} from './reaction'
+import { disposeBindingReactions, releaseBindingReactions } from './reaction'
 
 export class Tracker {
   private results: any
@@ -28,13 +23,11 @@ export class Tracker {
     if (ReactionStack.indexOf(this.track) === -1) {
       releaseBindingReactions(this.track)
       try {
-        batchStart()
         ReactionStack.push(this.track)
         this.results = tracker()
       } finally {
         ReactionStack.pop()
         this.track._boundary++
-        batchEnd()
         this.track._boundary = 0
       }
     }
