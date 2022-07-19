08fb21efb10fc332f80dba677248d4acb9a33fa7
janrywang <zhili.wzl@alibaba-inc.com>
2022-07-19 15:45:47 +0800

    fix(react): fix useAttach not work with react18 strict mode

--- packages/react/src/hooks/useAttach.ts
+++ packages/react/src/hooks/useAttach.ts
@@ -1,4 +1,3 @@
-import { useRef } from 'react'
 import { unstable_useCompatEffect } from '@formily/reactive-react'
 interface IRecycleTarget {
   onMount: () => void
@@ -6,13 +5,8 @@ interface IRecycleTarget {
 }
 
 export const useAttach = <T extends IRecycleTarget>(target: T): T => {
-  const oldRef = useRef(null)
   unstable_useCompatEffect(() => {
-    if (oldRef.current && oldRef.current !== target) {
-      oldRef.current.onUnmount()
-    }
     target.onMount()
-    oldRef.current = target
     return () => target.onUnmount()
   }, [target])
   return target
--- packages/reactive-react/src/hooks/useCompatEffect.ts
+++ packages/reactive-react/src/hooks/useCompatEffect.ts
@@ -1,20 +1,40 @@
 import { useEffect, useRef, EffectCallback, DependencyList } from 'react'
 import { immediate } from '../shared'
 
+const shallowEqual = (target: any, source: any) => {
+  const typeA = typeof target
+  const typeB = typeof source
+  if (typeA !== typeB) return false
+  const arrA = Array.isArray(target)
+  const arrB = Array.isArray(source)
+  if (arrA !== arrB) return false
+  if (arrA) {
+    if (target.length !== source.length) return false
+    return target.every((val, index) => val === source[index])
+  }
+  return target === source
+}
+
 export const useCompatEffect = (
   effect: EffectCallback,
   deps?: DependencyList
 ) => {
+  const depsRef = useRef<DependencyList>(null)
   const mountedRef = useRef(false)
   useEffect(() => {
     mountedRef.current = true
     const dispose = effect()
     return () => {
       mountedRef.current = false
+      if (!shallowEqual(depsRef.current, deps)) {
+        if (dispose) dispose()
+        return
+      }
       immediate(() => {
         if (mountedRef.current) return
         if (dispose) dispose()
       })
     }
   }, deps)
+  depsRef.current = deps
 }
