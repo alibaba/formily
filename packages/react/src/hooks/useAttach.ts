97898c055d2e98a7ec35644437eac11a7ddf1272
janrywang <zhili.wzl@alibaba-inc.com>
2022-07-19 14:31:49 +0800

    fix(react): fix useAttach not work with react18 strict mode

--- packages/react/src/hooks/useAttach.ts
+++ packages/react/src/hooks/useAttach.ts
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import { unstable_useCompatEffect } from '@formily/reactive-react'
 interface IRecycleTarget {
   onMount: () => void
@@ -5,8 +6,13 @@ interface IRecycleTarget {
 }
 
 export const useAttach = <T extends IRecycleTarget>(target: T): T => {
+  const oldRef = useRef(null)
   unstable_useCompatEffect(() => {
+    if (oldRef.current && oldRef.current !== target) {
+      oldRef.current.onUnmount()
+    }
     target.onMount()
+    oldRef.current = target
     return () => target.onUnmount()
   }, [target])
   return target
