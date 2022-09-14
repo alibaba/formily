5b204d0132570fc355d5079b19f454ab7d42fe69
tanghui.tang <tanghui.tang@alibaba-inc.com>
2022-09-14 22:09:47 +0800

    feat(next\antd): update moment timestamp test

--- packages/antd/__tests__/moment.spec.ts
+++ packages/antd/__tests__/moment.spec.ts
@@ -24,10 +24,8 @@ test('formatMomentValue is usable', () => {
   expect(formatMomentValue('12:11', 'HH:mm')).toBe('12:11')
   expect(formatMomentValue('12:11:11', 'HH:mm:ss')).toBe('12:11:11')
   expect(formatMomentValue(1663155911097, 'YYYY-MM-DD HH:mm:ss')).toBe(
-    '2022-09-14 19:45:11'
+    moment(1663155911097).format('YYYY-MM-DD HH:mm:ss')
   )
-  expect(formatMomentValue(1663155911097, 'YYYY-MM-DD')).toBe('2022-09-14')
-  expect(formatMomentValue(1663155911097, 'HH:mm:ss')).toBe('19:45:11')
   expect(
     formatMomentValue(
       ['2021-12-21 15:47:00', '2021-12-29 15:47:00'],
