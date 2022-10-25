00141fe79f770768af043699f9c19a6235f9028b
Janry <wangzhili56@126.com>
2022-10-26 00:07:50 +0800

    fix(react): fix throw react-dom error in react-native (#3491)

--- packages/react/src/shared/render.ts
+++ packages/react/src/shared/render.ts
@@ -1,16 +1,19 @@
 import React from 'react'
-import { createPortal } from 'react-dom'
 import { globalThisPolyfill } from '@formily/shared'
 
 const env = {
   portalDOM: null,
+  ReactDOM: null,
 }
 
 export const render = (element: React.ReactElement) => {
-  if (globalThisPolyfill['document']) {
+  if (globalThisPolyfill.navigator?.product === 'ReactNative') return null
+  if (globalThisPolyfill.document) {
     env.portalDOM =
       env.portalDOM || globalThisPolyfill['document'].createElement('div')
-    return createPortal(element, env.portalDOM)
+    env.ReactDOM = env.ReactDOM || require('react-dom')
+    //eslint-disable-next-line
+    return env.ReactDOM.createPortal(element, env.portalDOM)
   } else {
     return React.createElement('template', {}, element)
   }
