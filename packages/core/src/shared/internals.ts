7348d6080587cab9c48e9f99b6238bb074f28f4a
janrywang <wangzhili56@126.com>
2022-12-26 00:10:47 +0800

    refactor(core): revert initial values check logic

--- packages/core/src/shared/internals.ts
+++ packages/core/src/shared/internals.ts
@@ -1036,8 +1036,6 @@ export const allowAssignDefaultValue = (target: any, source: any) => {
   if (typeof target === typeof source) {
     if (target === '') return false
     if (target === 0) return false
-    if (Array.isArray(target)) return false
-    if (isPlainObj(target)) return false
   }
 
   const isEmptyTarget = target !== null && isEmpty(target)
