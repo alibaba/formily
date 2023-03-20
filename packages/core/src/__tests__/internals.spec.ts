d180c65bcb11469e0ce33181834ad62cdfe52e72
ZirkleTsing <835702580@qq.com>
2023-03-20 20:27:24 +0800

    test(core): adding missing tests

--- packages/core/src/__tests__/internals.spec.ts
+++ packages/core/src/__tests__/internals.spec.ts
@@ -5,6 +5,8 @@ import {
   deserialize,
   isHTMLInputEvent,
 } from '../shared/internals'
+import { createForm } from '../'
+import { attach } from './shared'
 
 test('getValuesFromEvent', () => {
   expect(getValuesFromEvent([{ target: { value: 123 } }])).toEqual([123])
@@ -26,6 +28,62 @@ test('patchFieldStates', () => {
     { type: 'update3' as any, address: 'aaa', payload: null },
   ])
   expect(fields).toEqual({})
+})
+
+test('patchFieldStates should be sequence', () => {
+  const form = attach(createForm())
+  attach(
+    form.createArrayField({
+      name: 'array',
+    })
+  )
+  attach(
+    form.createField({
+      name: 'input',
+      basePath: 'array.0',
+    })
+  )
+  attach(
+    form.createField({
+      name: 'input',
+      basePath: 'array.1',
+    })
+  )
+  const before = Object.keys(form.fields)
+  form.fields['array'].move(1, 0)
+  const after = Object.keys(form.fields)
+  expect(after).toEqual(before)
+
+  const form2 = attach(createForm())
+  attach(
+    form2.createField({
+      name: 'field1',
+      title: 'Field 1',
+    })
+  )
+  attach(
+    form2.createField({
+      name: 'field2',
+      title: 'Field 1',
+    })
+  )
+
+  patchFieldStates(form2.fields, [
+    {
+      type: 'update',
+      address: 'field2',
+      oldAddress: 'field1',
+      payload: form2.field1,
+    },
+    {
+      type: 'update',
+      address: 'field1',
+      oldAddress: 'field2',
+      payload: form2.field2,
+    },
+  ])
+
+  expect(Object.keys(form2.fields)).toEqual(['field1', 'field2'])
 })
 
 test('deserialize', () => {
