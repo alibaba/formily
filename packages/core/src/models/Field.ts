d3bfbe3753d2c9af5b2ce9081b805f55359b8031
gwsbhqt <gwsbhqt@163.com>
2023-04-10 15:04:45 +0800

    fix: field hidden with null value

--- packages/core/src/models/Field.ts
+++ packages/core/src/models/Field.ts
@@ -246,7 +246,7 @@ export class Field<
         (display) => {
           const value = this.value
           if (display !== 'none') {
-            if (!isValid(value)) {
+            if (value === undefined && this.caches.value !== undefined) {
               this.setValue(this.caches.value)
               this.caches.value = undefined
             }
