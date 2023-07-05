e582c38c2456e556e746c0a4d64a4f3b4b24b5d1
huangcheng <huangcheng.lq@bytedance.com>
2023-07-05 13:25:44 +0800

    fix: remove unneeded import

--- packages/reactive/src/__tests__/tracker.spec.ts
+++ packages/reactive/src/__tests__/tracker.spec.ts
@@ -1,5 +1,4 @@
-import { Tracker, observable, autorun } from '../'
-import expect from 'expect'
+import { Tracker, observable } from '../'
 
 test('base tracker', () => {
   const obs = observable<any>({})
