c94da3fee98833b42d8e78cc1a8c364fead1b40a
yiyunwan <48950552+yiyunwan@users.noreply.github.com>
2023-12-07 10:41:50 +0800

    fix: fix vue2 array reactive bug (#4042)
    * fix: fix vue2 array reactive bug
    
    * test: add observable __proto__ test case

--- packages/reactive/src/handlers.ts
+++ packages/reactive/src/handlers.ts
@@ -7,13 +7,12 @@ import { isObservable, isSupportObservable } from './externals'
 import { createObservable } from './internals'
 
 const wellKnownSymbols = new Set(
-  Object.getOwnPropertyNames(Symbol)
-    .reduce((buf: Symbol[], key) => {
-      if (key === 'arguments' || key === 'caller') return buf    
-      const value = Symbol[key]
-      if (typeof value === 'symbol') return buf.concat(value)
-      return buf
-    }, [])
+  Object.getOwnPropertyNames(Symbol).reduce((buf: Symbol[], key) => {
+    if (key === 'arguments' || key === 'caller') return buf
+    const value = Symbol[key]
+    if (typeof value === 'symbol') return buf.concat(value)
+    return buf
+  }, [])
 )
 
 const hasOwnProperty = Object.prototype.hasOwnProperty
@@ -201,6 +200,11 @@ export const baseHandlers: ProxyHandler<any> = {
     return keys
   },
   set(target, key, value, receiver) {
+    // vue2中有对数组原型重写，因此需去除此处proxy
+    if (key === '__proto__') {
+      target[key] = value
+      return true
+    }
     const hadKey = hasOwnProperty.call(target, key)
     const newValue = createObservable(target, key, value)
     const oldValue = target[key]
