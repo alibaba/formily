e20312d8d2127d8a041ca1105d9e35a1a305b10c
lala2333 <953716500@qq.com>
2025-01-21 17:39:11 +0800

    fix(ele): select preview-text error when use datasource

--- packages/element/src/preview-text/index.ts
+++ packages/element/src/preview-text/index.ts
@@ -67,11 +67,6 @@ const Select = observer(
       const fieldRef = useField<Field>()
       const field = fieldRef.value
       const props = attrs as unknown as SelectProps
-      const dataSource: any[] = field?.dataSource?.length
-        ? field.dataSource
-        : props?.options?.length
-        ? props.options
-        : []
       const placeholder = usePlaceholder()
       const getSelected = () => {
         const value = props.value
@@ -86,6 +81,11 @@ const Select = observer(
 
       const getLabels = () => {
         const selected = getSelected()
+        const dataSource: any[] = field?.dataSource?.length
+          ? field.dataSource
+          : props?.options?.length
+          ? props.options
+          : []
         if (!selected.length) {
           return h(
             Tag,
