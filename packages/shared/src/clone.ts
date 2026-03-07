77f8aa2c59f74886cc8971d41ab59b14122c1360
gongyanyu <nanyuantingfeng@163.com>
2026-03-07 16:21:01 +0800

    feat(shared): 添加文档说明

--- packages/shared/src/clone.ts
+++ packages/shared/src/clone.ts
@@ -13,6 +13,9 @@ export const shallowClone = (values: any) => {
     if (values['_isAMomentObject']) {
       return values
     }
+
+    // >= dayjs@1.11.10 has `$isDayjsObject` check logic
+    // following the `moment` convention, return the original object directly
     if (values['$isDayjsObject']) {
       return values
     }
@@ -22,6 +25,9 @@ export const shallowClone = (values: any) => {
     if (isFn(values['toJS'])) {
       return values
     }
+    // < dayjs@1.11.10 has `clone` method, directly return a new object from `clone`, which won't cause logic issues.
+    // This check needs to be done before `toJSON` to avoid executing dayjs's `toJSON` method, which could cause type errors
+    // This logic check also applies to other similar objects, because the current method expects the return value to be the same as the input
     if (isFn(values['clone'])) {
       return values['clone']()
     }
